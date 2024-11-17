9b7863f5f0f13b5b2c58b29234ba319b32c5ef84
Rémi Angot <remi.angot@gmail.com>
2024-11-17 09:35:23 +0100

    Vite options de chunking

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,38 @@ export default defineConfig({
   base: '/alea/',
   build: {
     target: ['es2020', 'edge88', 'firefox78', 'chrome87', 'safari14'],
-    sourcemap: true
+    sourcemap: true,
+    // À partir du 16/11 le build est devenu impossible sans options de chunking
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          // Pour les dépendances pnpm
+          if (id.includes('.pnpm')) {
+            // Extraire le vrai nom du package
+            const match = id.match(/.pnpm\/(.*?)@/)
+            if (match && match[1]) {
+              // Nettoyer le nom du package
+              const pkgName = match[1]
+                .replace(/@/g, '')
+                .replace(/\//g, '-')
+              return `vendors/${pkgName}`
+            }
+          }
+
+          // Pour les dépendances normales
+          if (id.includes('node_modules')) {
+            const module = id.split('node_modules/')[1].split('/')[0]
+            return `vendors/${module.replace(/[@\/]/g, '-')}`
+          }
+
+          // Pour les JSON
+          if (id.endsWith('.json')) {
+            const jsonName = id.split('/').pop().replace('.json', '')
+            return `json/${jsonName}`
+          }
+        }
+      }
+    }
   },
   server: (process.env.CI ? { port: 80, watch: null } : { port: 5173 }),
   define: {
