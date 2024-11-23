8d6510bf9db6983645025d76f947f71e5a22f095
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2024-11-23 19:59:04 +0100

    Nouvel exo DNB aleatoirisé 3L14DNB-0

---
+++ src/exercices/3e/3L14DNB-0.ts
@@ -0,0 +1,244 @@
+import { createList } from '../../lib/format/lists'
+import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
+import { ecritureAlgebrique, ecritureAlgebriqueSauf1 } from '../../lib/outils/ecritures'
+import { texteEnBoite } from '../../lib/outils/embellissements'
+import { listeDesDiviseurs, pgcd, texFactorisation } from '../../lib/outils/primalite'
+import { context } from '../../modules/context'
+import FractionEtendue from '../../modules/FractionEtendue'
+import { randint } from '../../modules/outils'
+import { scratchblock } from '../../modules/scratchblock'
+import ExerciceBrevetA from '../ExerciceBrevetA'
+
+export const uuid = '6c592'
+export const refs = {
+  'fr-fr': ['3L14DNB-0'],
+  'fr-ch': []
+}
+export const titre = 'Exercice 4 (Antilles-Guyane 06/2024)'
+export const dateDePublication = '15/11/2024'
+const enonceOriginal = `\\medskip
+
+On considère le programme de calcul ci-dessous :
+
+\\begin{center}
+// \\begin{tabular}{|l|}\\hline
+$\\bullet~$Choisir un nombre\\
+$\\bullet~$Mettre ce nombre au carré\\
+$\\bullet~$Soustraire le triple du nombre de départ\\
+// $\\bullet~$Soustraire 4\\ \\hline
+\\end{tabular}
+\\end{center}
+
+\\smallskip
+
+\\begin{enumerate}
+\\item Montrer que si on choisit 5 comme nombre de départ, le résultat du programme est 6.
+\\item On choisit $x$ comme nombre de départ.
+
+Exprimer le résultat du programme en fonction de $x$.
+\\item Vérifier que l'on peut écrire ce résultat sous la forme $(x+ 1)(x - 4)$.
+\\item Déterminer les nombres à choisir au départ pour que le résultat du programme soit 0.
+\\item Juliette a écrit le programme ci-dessous :
+
+\\begin{scratch}[num blocks]
+// \\blockinit{quand \\greenflag est cliqué}
+// \\blockmove{demander \\ovalnum{Choisir un nombre} et attendre}
+\\blockvariable{mettre \\selectmenu{x} à \ovalmove{réponse}}
+// // // // \\blockvariable{mettre \\selectmenu{y} à \ovaloperator{\\ovalnum{\\ldots}*\\ovalnum{\\ldots}}}
+// // \\blockvariable{mettre \\selectmenu{z} à \\ovaloperator{\\ovalnum{3}*\\ovalnum{x}}}
+// // // // \\blockvariable{mettre \\selectmenu{Résultat} à \\ovaloperator{\\ovalnum{\\ldots}-\\ovalnum{\\ldots} - 4}}
+\\blocklook{dire \\ovalnum{Résultat } pendant \\ovalnum{5}secondes}
+\\end{scratch}
+\\end{enumerate}
+
+\\medskip
+
+Recopier et compléter sur la copie les lignes 4 et 6 du programme afin que celui-ci corresponde au programme de calcul encadré.
+
+\\bigskip
+
+
+`
+const correctionOriginale = `
+\\medskip
+
+On considère le programme de calcul ci-dessous :
+
+\\begin{center}
+\\begin{tabular}{|l|}\\hline
+$\\bullet~$Choisir un nombre\\\\
+$\\bullet~$Mettre ce nombre au carré\\\\
+$\\bullet~$Soustraire le triple du nombre de départ\\\\
+$\\bullet~$Soustraire 4\\\\ \\hline
+\\end{tabular}
+\\end{center}
+
+\\smallskip
+
+\\begin{enumerate}
+\\item %Montrer que si on choisit 5 comme nombre de départ, le résultat du programme est 6.
+On a successivement : $5 \\to 5^2 = 25 \\to 25 - 3 \\times 5 = 10  \\to, 10 - 4 = 6$.
+\\item %On choisit $x$ comme nombre de départ.
+De même avec $x$ au départ : 
+
+$x \\to x^2 \\to x^2 - 3x \\to x^2 - 3x - 4$.
+%Exprimer le résultat du programme en fonction de $x$.
+\\item %Vérifier que l'on peut écrire ce résultat sous la forme $(x+ 1)(x - 4)$
+On développe $(x+ 1)(x - 4) = x^2 - 4x + x  - 4 = x^2 - 3x - 4 $. On retrouve l'expression de la question 2.
+
+On a donc $x^2 - 3x - 4 = (x + 1)(x - 4)$.
+\\item %Déterminer les nombres à choisir au départ pour que le résultat du programme soit 0.
+Il faut trouver un ou des nombres $x$ tels que $x^2 - 3x - 4 = 0$ ou d'après la question précédente tels que :
+
+$x + 1)(x - 4) = 0$.
+
+Un produit de facteurs est nul si l'un des facteurs est nul , soit 
+
+$\\left\\{\\begin{array}{l c l}
+x +1&=&0\\\\
+&\\text{ou}&\\\\
+x - 4&=&0
+\\end{array}\\right.$ d'où $\\left\\{\\begin{array}{l c l}
+x &=&- 1\\\\
+&\\text{ou}&\\\\
+x &=& 4
+\\end{array}\\right.$.
+
+Il y a donc deux nombres qui donnent finalement 0 : ce sont $- 1$ et 4.
+\\item %Juliette a écrit le programme ci-dessous :
+
+%\\begin{scratch}[num blocks]
+%\\blockinit{quand \\greenflag est cliqué}
+%\\blockmove{demander \\ovalnum{Choisir un nombre} et attendre}
+%\\blockvariable{mettre \\selectmenu{x} à \\ovalmove{réponse}}
+%\\blockvariable{mettre \\selectmenu{y} à \\ovaloperator{\\ovalnum{\\ldots}*\\ovalnum{\\ldots}}}
+%\\blockvariable{mettre \\selectmenu{z} à \\ovaloperator{\\ovalnum{3}*\\ovalnum{x}}}
+%\\blockvariable{mettre \\selectmenu{Résultat} à \\ovaloperator{\\ovalnum{\\ldots}-\\ovalnum{\\ldots} - 4}}
+%\\blocklook{dire \\ovalnum{Résultat } pendant \\ovalnum{5}secondes}
+%\\end{scratch}
+Juliette doit compléter en ligne 4 et 6 :
+
+\\begin{scratch}
+\\blockvariable{mettre \\selectmenu{y} à \\ovaloperator{\\ovalnum{x}*\\ovalnum{x}}}
+\\blockvariable{mettre \\selectmenu{Résultat} à \\ovaloperator{\\ovalnum{y}-\\ovalnum{z} - 4}}
+\\end{scratch}
+\\end{enumerate}
+
+\\medskip
+
+%Recopier et compléter sur la copie les lignes 4 et 6 du programme afin que celui-ci corresponde au programme de calcul encadré.
+
+\\bigskip
+`
+/**
+ * @Author Jean-Claude Lhote
+ * Cet exerice exploite la nouvelle classe d'exercice que j'ai conçue pour les sujets de brevet
+ * Il s'agit d'un exercice de type Brevet Aléatoirisé
+ * La méthode privée appliquerLesValeurs permet de générer les valeurs aléatoires et de construire l'énoncé et la correction
+ * La méthode versionOriginale permet de générer les valeurs de l'exercice telles qu'elles sont dans le sujet original
+ * La méthode versionAleatoire permet de générer des valeurs aléatoires pour l'exercice
+ */
+export default class Exercice3A10DNB0 extends ExerciceBrevetA {
+  constructor () {
+    super()
+    this.besoinFormulaireCaseACocher = ['Sujet original', false]
+    this.sup = false
+    this.nbQuestionsModifiable = true
+    this.versionAleatoire()
+  }
+
+  private appliquerLesValeurs (facteur: 1|2|3|4, retrait: number, depart: number, a:number, b:number) {
+    const facteurs = {
+      1: 'nombre de départ',
+      2: 'double du nombre de départ',
+      3: 'triple du nombre de départ',
+      4: 'quadruple du nombre de départ'
+    }
+    const nbFois = facteurs[facteur]
+    const f = (x:number) => (x + a) * (x - b)
+    console.log(`a=${a} ; b=${b} ; depart=${depart} ; f(depart)=${f(depart)}`)
+    // enonce
+    let enonce = 'On considère le programme de calcul ci-dessous :<br>'
+    enonce += texteEnBoite(createList({
+      items: [
+        'Choisir un nombre',
+        'Mettre ce nombre au carré',
+        `Soustraire le ${nbFois}`,
+        `Soustraire ${retrait}`
+      ],
+      style: 'fleches'
+    }))
+    let correction = enonce
+    let texteScratch = `\\begin{scratch}[${context.isHtml ? 'print,' : ''}fill,blocks,scale=0.8]\n`
+    texteScratch += '\\blockinit{quand \\greenflag est cliqué}\n'
+    texteScratch += '\\blockmove{demander \\ovalnum{Choisir un nombre} et attendre}\n'
+    texteScratch += '\\blockvariable{mettre \\selectmenu{x} à \\ovalmove{réponse}}\n'
+    texteScratch += '\\blockvariable{mettre \\selectmenu{y} à \\ovaloperator{\\ovalnum{...}*\\ovalnum{...}}}\n'
+    texteScratch += `\\blockvariable{mettre \\selectmenu{z} à \\ovaloperator{\\ovalnum{${facteur}}*\\ovalnum{x}}}\n`
+    texteScratch += `\\blockvariable{mettre \\selectmenu{Résultat} à \\ovaloperator{\\ovaloperator{\\ovalnum{...}-\\ovalnum{...}} -\\ovalnum{${retrait}} }}\n`
+    texteScratch += '\\blocklook{dire \\ovalnum{Résultat } pendant \\ovalnum{5}secondes}\n'
+    texteScratch += '\\end{scratch}\n'
+
+    const listeQuestions = createList({
+      items: [
+        `Montrer que si on choisit $${depart}$ comme nombre de départ, le résultat du programme est $${f(depart)}$.`,
+        'On choisit $x$ comme nombre de départ. Exprimer le résultat du programme en fonction de $x$.',
+        `Vérifier que l\'on peut écrire ce résultat sous la forme $(x+${a})(x-${b})$.`,
+        'Déterminer les nombres à choisir au départ pour que le résultat du programme soit 0.',
+            `Juliette a écrit le programme ci-dessous :<br>
+            ${scratchblock(texteScratch)}<br>
+            Recopier et compléter sur la copie les lignes 4 et 6 du programme afin que celui-ci corresponde au programme de calcul encadré.`
+      ],
+      style: 'nombres'
+    })
+    enonce += listeQuestions
+    // correction
+
+    let texteScratch2 = `\\begin{scratch}[${context.isHtml ? 'print,' : ''}fill,blocks,scale=0.8]\n`
+    texteScratch2 += '\\blockvariable{mettre \\selectmenu{y} à \\ovaloperator{\\ovalnum{x}*\\ovalnum{x}}}\n'
+    texteScratch2 += `\\blockvariable{mettre \\selectmenu{Résultat} à \\ovaloperator{\\ovaloperator{\\ovalnum{y}-\\ovalnum{z}} -\\ovalnum{${retrait}} }}\n`
+    texteScratch2 += '\\end{scratch}\n'
+
+    const listeCorrections = createList({
+      items: [
+              `On a successivement : $${depart} \\to ${depart}^2 = ${depart * depart} \\to ${depart * depart} - ${facteur} \\times ${depart} = ${depart * depart - facteur * depart}  \\to, ${depart * depart - facteur * depart} - ${retrait} = ${f(depart)}$.`,
+              `De même avec $x$ au départ : <br>
+              $x \\to x^2 \\to x^2 - ${facteur}x \\to x^2 - ${facteur}x - ${retrait}$.`,
+              `On développe $(x+${a})(x-${b}) = x^2 ${ecritureAlgebriqueSauf1(-b)}x${ecritureAlgebriqueSauf1(a)}x${ecritureAlgebrique(-b * a)}=x^2-${facteur}x-${retrait}$.<br>On retrouve l'expression de la question 2.<br>
+              On a donc $x^2 - ${facteur}x - ${retrait} = (x+${a})(x-${b})$.`,
+              `Il faut trouver un ou des nombres $x$ tels que $x^2-${facteur}x-${retrait}=0$ ou d'après la question précédente tels que :<br>
+              $(x+${a})(x-${b})=0$.<br>
+              Un produit de facteurs est nul si l'un des facteurs est nul , soit <br>
+              $\\left\\{\\begin{array}{l c l}
+              x+${a}&=&0\\\\
+              &\\text{ou}&\\\\
+              x-${b}&=&0
+              \\end{array}\\right.$ d'où $\\left\\{\\begin{array}{l c l}
+              x&=&${-a}\\\\
+              &\\text{ou}&\\\\
+              x&=& ${b}
+              \\end{array}\\right.$.`,
+              `Juliette doit compléter en ligne 4 et 6 :<br>
+              ${scratchblock(texteScratch2)}`
+      ],
+      style: 'nombres'
+    })
+    correction += `<br><br>${listeCorrections}`
+
+    this.enonce = enonce
+    this.correction = correction
+  }
+
+  versionOriginale: () => void = () => {
+    this.appliquerLesValeurs(3, 4, 5, 1, 4)
+  }
+
+  versionAleatoire: () => void = () => {
+    const a = randint(1, 3)
+    const b = a + choice([1, 2, 3, 4])
+    const facteur = b - a
+    const retrait = a * b
+    const depart = randint(1, 10, [a, b])
+    this.appliquerLesValeurs(facteur as 1|2|3|4, retrait, depart, a, b)
+  }
+}
