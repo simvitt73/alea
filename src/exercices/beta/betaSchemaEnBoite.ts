956c4b8c249ee94459dfca25ff1f8cd8f34db279
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2024-12-14 06:35:50 +0000

    SchemaEnBoite

---
+++ src/exercices/beta/betaSchemaEnBoite.ts
@@ -0,0 +1,134 @@
+import { prenomF, prenomM } from '../../lib/outils/Personne'
+import SchemaEnBoite from '../../lib/outils/SchemaEnBoite'
+import { texNombre } from '../../lib/outils/texNombre'
+import { gestionnaireFormulaireTexte, randint } from '../../modules/outils'
+import Exercice from '../Exercice'
+
+export const titre = 'Schemas en boite'
+export const uuid = 'sebseb'
+
+export default class BetaSchemaEnBoite extends Exercice {
+  constructor () {
+    super()
+    this.nbQuestions = 4
+    this.besoinFormulaireTexte = ['Types de problèmes', [
+      'nombres séparés par des tirets',
+      '1 : Somme dont on cherche le résultat',
+      '2 : Soustraction dont on cherche un terme',
+      '3 : Multiplication dont on cherche le résultat',
+      '4 : Division partition ou quotition exacte',
+      '5 : Division dont on cherche le reste',
+      '6 : Division dont on cherche le dividende',
+      '7 : Mélange'
+    ].join('\n')]
+    this.sup = '1'
+  }
+
+  nouvelleVersion (numeroExercice?: number): void {
+    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
+      const type: number[] = gestionnaireFormulaireTexte({ nbQuestions: this.nbQuestions, saisie: this.sup, min: 1, max: 6, defaut: 1, melange: 7 }).map(Number)
+      const lea = prenomF(1)
+      const eric = prenomM(1)
+      let texte: string
+      let seb: SchemaEnBoite
+      let nb1: number
+      let nb2: number
+      switch (type[i]) {
+        case 1: { // Addition dont on cherche le résultat
+          const hl1 = randint(3, 5)
+          const hl2 = 8 - hl1
+          const coef = randint(2, 8)
+          nb1 = hl1 * coef
+          nb2 = hl2 * coef
+          // const somme = nb1 + nb2
+
+          texte = `${lea} a ${nb1} bonbons et ${eric} a ${nb2} bonbons. Combien ont-ils de bonbons en tout ?`
+          seb = SchemaEnBoite.addition(nb1, nb2, 2) // J'ai mis 2 en précision pour que les cases soient plus grandes
+        }
+          break
+        case 2: { // Soustraction dont on cherche un terme
+          const hl1 = randint(3, 5)
+          const hl2 = 8 - hl1
+          const coef = randint(2, 8)
+          nb1 = hl1 * coef
+          nb2 = hl2 * coef
+          const somme = nb1 + nb2
+          switch (randint(1, 3)) {
+            case 1:
+              texte = `${lea} a donné ${nb1} bonbons à ${eric} et maintenant il lui en reste ${nb2}. Combien ${lea} avait-elle de bonbons au départ ?`
+              seb = SchemaEnBoite.addition(nb1, nb2, 2)
+
+              break
+            case 2:
+              {
+                const hl1 = randint(3, 5)
+                const hl2 = 8 - hl1
+                const coef = randint(2, 8)
+                nb1 = hl1 * coef
+                nb2 = hl2 * coef
+                const somme = nb1 + nb2
+                texte = `${lea} a ${nb1} bonbons et, avec ${eric}, ils en ont ${somme}. Combien ${eric} a-t-il de bonbons ?`
+                seb = SchemaEnBoite.soustraction(somme, undefined, nb1, 2)
+              }
+              break
+            default:
+              texte = `${lea} a ${somme} bonbons. Après en avoir donné à ${eric}, il lui en reste ${nb1}. Combien de bonbons a-t-elle donné ?`
+              seb = SchemaEnBoite.soustraction(somme, nb1, undefined, 2)
+          }
+          break
+        }
+        case 3: { // Multiplication dont on cherche le résultat
+          const nbFois = randint(15, 30)
+          nb1 = randint(2, 8)
+          nb2 = nb1 * nbFois
+
+          texte = `${lea} a distribué ${nb1} bonbons à chacun de ses ${nbFois} élèves. Combien de bonbons a-t-elle distribué en tout ?`
+          seb = SchemaEnBoite.multiplication(nb1, nbFois, 2) // J'ai mis 2 en précision pour que les cases soient plus grandes
+        }
+          break
+        case 4: // Division partition ou quotition exacte
+          switch (randint(1, 2)) {
+            case 1: {
+              const nbFois = randint(15, 30)
+              nb1 = randint(2, 8)
+              nb2 = nb1 * nbFois
+              texte = `${lea} avait ${nb2} bonbons. elle les a distribué équitablement à chacun de ses ${nbFois} élèves. Combien de bonbons a reçu chaque élève ?`
+              seb = SchemaEnBoite.division(nb2, nbFois, undefined, 2)
+            }
+              break
+            default: {
+              const nbFois = randint(15, 30)
+              nb1 = randint(2, 8)
+              nb2 = nb1 * nbFois
+              texte = `${lea} a distribué ${nb1} bonbons à chacun de ses élèves. En tout, elle a distribué ${nb2} bonbons. Combien d'élèves a-t-elle ?`
+              seb = SchemaEnBoite.division(nb2, undefined, nb1, 2)
+            }
+          }
+          break
+
+        case 5: { // Division dont on cherche le reste
+          const nbFois = randint(15, 30)
+          nb1 = randint(2, 8)
+          nb2 = nb1 * nbFois + randint(1, nb1 - 1)
+          texte = `${lea} avait ${nb2} bonbons. elle a distribué ${nb1} bonbons à chacun de ses ${nbFois} élèves. Combien de bonbons lui reste-t-il ?`
+          seb = SchemaEnBoite.divisionAvecReste(nb1, nb2, String(nbFois), 0, '?')
+        }
+          break
+        default: { // Division dont on cherche le dividende
+          const nbFois = randint(15, 30)
+          nb1 = randint(2, 8)
+          const reste = randint(1, nb1 - 1)
+          nb2 = nb1 * nbFois + reste
+          texte = `${lea} a distribué ${nb1} bonbons à chacun de ses ${nbFois} élèves. Il lui en reste ${reste}. Combien en avait-elle avant la distribution ?`
+          seb = SchemaEnBoite.divisionAvecReste(nb1, undefined, String(nbFois), 0, `$${texNombre(reste, 0)}$`)
+        }
+      }
+      if (this.questionJamaisPosee(i, nb1, nb2)) {
+        this.listeQuestions.push(`${texte}<br><br>${seb.display()}`)
+        this.listeCorrections.push('') // TODO
+        i++
+      }
+      cpt++
+    }
+  }
+}
--- src/lib/outils/SchemaEnBoite.ts
+++ src/lib/outils/SchemaEnBoite.ts
@@ -1,43 +1,278 @@
 import { context } from '../../modules/context'
+import './ShemasEnBoite.css'
+import { texNombre } from './texNombre'
 
+type AccoladeType = {
+  start: number, // colonne de début (1 = première colonne)
+  end: number, // colonne de fin = start + longueur de l'accolade
+  text: string
+}
 type BarreSchemaType = {
-    color: string
-    longueur: number
-    content: string
+  color: string
+  length: number
+  content: string
 }
 type LigneSchemaType = BarreSchemaType[]
+type LigneAccoladeType = AccoladeType[]
 export default class SchemaEnBoite {
-  haut: LigneSchemaType
-  bas: LigneSchemaType
+  braceT?: LigneAccoladeType
+  braceB?: LigneAccoladeType
+  top: LigneSchemaType
+  bottom: LigneSchemaType
 
-  constructor ({ haut, bas }: { haut: LigneSchemaType, bas: LigneSchemaType } = { haut: [], bas: [] }) {
-    this.haut = haut
-    this.bas = bas
+  constructor ({ top, bottom, braceB, braceT }: { top: LigneSchemaType, bottom: LigneSchemaType, braceB?: LigneAccoladeType, braceT?: LigneAccoladeType } = { top: [], bottom: [] }) {
+    this.top = top
+    this.bottom = bottom
+    if (braceB) {
+      this.braceB = braceB
+    }
+    if (braceT) {
+      this.braceT = braceT
+    }
   }
 
   concat (boite: SchemaEnBoite): SchemaEnBoite {
-    return new SchemaEnBoite({ haut: this.haut.concat(boite.haut) ?? [], bas: this.bas.concat(boite.bas) ?? [] })
+    return new SchemaEnBoite({ top: this.top.concat(boite.top) ?? [], bottom: this.bottom.concat(boite.bottom) ?? [], braceB: this.braceB?.concat(boite.braceB ?? []) ?? [], braceT: this.braceT?.concat(boite.braceT ?? []) ?? [] })
   }
 
   display (): string {
     if (context.isHtml) {
+      let ligneAccoladeH = ''
+      if (this.braceT) {
+        for (let k = 0; k < this.braceT.length; k++) {
+          const accolade = this.braceT[k]
+          const start = accolade.start
+          const end = accolade.end
+          const texte = accolade.text
+          if (start != null && end != null && texte != null) {
+            ligneAccoladeH +=
+            `<div class="SchemaItem" style="grid-row: 1; grid-column-start: ${start}; grid-column-end: ${end}; text-align:center; border: none">
+            <div class="latexAccoladeTop">$${texte}$</div>
+            <div class="curly-brace">
+            <div class="brace right"></div>
+            <div class="brace left"></div>
+            </div>
+            </div>\n`
+          }
+        }
+      }
       let ligne1 = ''
       let start = 1
-      for (let k = 0; k < this.haut.length; k++) {
-        ligne1 += `<div class="SchemaItem" style="grid-row: 1; grid-column-start: ${start}; grid-column-end: ${start + this.haut[k].longueur}; background-color:${this.haut[k].color}; text-align:center;">${this.haut[k].content}</div>\n`
-        start += this.haut[k].longueur
+      for (let k = 0; k < this.top.length; k++) {
+        ligne1 += `<div class="SchemaItem" style="grid-row: 2; grid-column-start: ${start}; grid-column-end: ${start + this.top[k].length}; background-color:${this.top[k].color}; text-align:center;">${this.top[k].content}</div>\n`
+        start += this.top[k].length
       }
       let ligne2 = ''
       start = 1
-      for (let k = 0; k < this.bas.length; k++) {
-        ligne2 += `<div class="SchemaItem" style="grid-row: 2; grid-column-start: ${start}; grid-column-end: ${start + this.bas[k].longueur}; background-color:${this.bas[k].color}; text-align:center;">${this.bas[k].content}</div>\n`
-        start += this.bas[k].longueur
+      for (let k = 0; k < this.bottom.length; k++) {
+        ligne2 += `<div class="SchemaItem" style="grid-row: 3; grid-column-start: ${start}; grid-column-end: ${start + this.bottom[k].length}; background-color:${this.bottom[k].color}; text-align:center;">${this.bottom[k].content}</div>\n`
+        start += this.bottom[k].length
       }
-      return `<div class="SchemaContainer">${ligne1}\n${ligne2}</div>`
-    } else {
-      const ligne1 = this.haut.map(barre => `${barre.color} ${barre.longueur} "${barre.content}"`).join(' ') ?? ''
-      const ligne2 = this.bas.map(barre => `${barre.color} ${barre.longueur} "${barre.content}"`).join(' ') ?? ''
-      return `\\ModeleBarre{${ligne1}}{${ligne2}}`
+      let ligneAccoladeB = ''
+      if (this.braceB) {
+        for (let k = 0; k < this.braceB.length; k++) {
+          const brace = this.braceB[k]
+          const start = brace.start
+          const end = brace.end
+          const texte = brace.text
+          if (start != null && end != null && texte != null) {
+            ligneAccoladeB +=
+            `<div class="SchemaItem" style="grid-row: 4; grid-column-start: ${start}; grid-column-end: ${end}; text-align:center; border: none">
+            <div class="curly-brace">
+            <div class="brace left"></div>
+            <div class="brace right"></div>
+            </div>
+             <div class="latexAccoladeBottom">$${texte}$</div>
+            </div>\n`
+          }
+        }
+      }
+      return `<div class="SchemaContainer">${ligneAccoladeH}\n${ligne1}\n${ligne2}\n${ligneAccoladeB}</div>`
+    } else { // latex
+      let latex = '\\begin{tikzpicture}\n'
+      if (this.braceT) {
+        for (let k = 0; k < this.braceT.length; k++) {
+          const brace = this.braceT[k]
+          const start = (brace.start - 1) / 2
+          const end = (brace.end - 1) / 2
+          const texte = brace.text
+          if (start != null && end != null && texte != null) {
+            latex += `\\draw[decorate,decoration={brace,amplitude=10pt},xshift=0pt,yshift=0pt] (${start},3) -- node[above=10pt] {$${texte}$} (${end},3);\n`
+          }
+        }
+      }
+      let start = 0
+      for (let k = 0; k < this.top.length; k++) {
+        const barre = this.top[k]
+        const end = start + barre.length / 2
+        latex += `\\draw[fill=${barre.color}] (${start},3) rectangle (${end},2) node[pos=.5] {$${barre.content}$};\n`
+        start += barre.length / 2
+      }
+      start = 0
+      for (let k = 0; k < this.bottom.length; k++) {
+        const barre = this.bottom[k]
+        const end = start + barre.length / 2
+        latex += `\\draw[fill=${barre.color}] (${start},2) rectangle (${end},1) node[pos=.5] {$${barre.content}$};\n`
+        start += barre.length / 2
+      }
+      if (this.braceB) {
+        for (let k = 0; k < this.braceB.length; k++) {
+          const brace = this.braceB[k]
+          const start = (brace.start - 1) / 2
+          const end = (brace.end - 1) / 2
+          const texte = brace.text
+          if (start != null && end != null && texte != null) {
+            latex += `\\draw[decorate,decoration={brace,mirror,amplitude=10pt},xshift=0pt,yshift=0pt] (${start},1) -- node[below=10pt] {$${texte}$} (${end},1);\n`
+          }
+        }
+      }
+      latex += '\\end{tikzpicture}'
+      return latex
     }
+  }
+
+  /**
+   *
+   * @param nb1 Le premier facteur (celui de la ligne du haut)
+   * @param nb2 Le deuxième facteur (celui de l'accolade)
+   * @param precision Le nombre de décimales
+   * @returns
+   */
+  static multiplication (nb1: number, nb2: number, precision: number): SchemaEnBoite {
+    const seb = new SchemaEnBoite({
+      braceT: [
+        {
+          start: 1,
+          end: 8 + precision * 2,
+          text: `${nb2}\\text{ fois}`,
+        }
+      ],
+      top: [
+        { color: 'lightgray', length: 2 + precision, content: `$${texNombre(nb1, precision)}$` },
+        { color: 'lightgray', length: 3, content: '...' },
+        { color: 'lightgray', length: 2 + precision, content: `$${texNombre(nb1, precision)}$` },
+      ],
+      bottom: [
+        { color: 'white', length: 7 + precision * 2, content: '?' },
+      ]
+    })
+    return seb
+  }
+
+  /**
+ *
+ * @param dividende Le dividende (écrit dans la case du dessous)
+ * @param nbFois Le diviseur (nombre entier de fois écrit sur l'accolade)
+ * @param diviseur Le diviseur (écrit dans les cases du dessus)
+ * @param precision nombre de chiffres après la virgule du dividende et du diviseur
+ * @returns
+ */
+  static division (dividende: number | undefined, nbFois: number | undefined, diviseur: number | undefined, precision: number): SchemaEnBoite {
+    const seb = new SchemaEnBoite({
+      braceT: [
+        {
+          start: 1,
+          end: 8 + 2 * precision,
+          text: nbFois == null ? '?' : `\\text{${texNombre(nbFois, 0)} fois}`,
+        }
+      ],
+      top: [
+        { color: 'lightgray', length: 2 + precision, content: diviseur == null ? '?' : `$${texNombre(diviseur, precision)}$` },
+        { color: 'lightgray', length: 3, content: '...' },
+        { color: 'lightgray', length: 2 + precision, content: diviseur == null ? '?' : `$${texNombre(diviseur, precision)}$` },
+      ],
+      bottom: [
+        { color: 'white', length: 7 + 2 * precision, content: dividende == null ? '?' : `$${texNombre(dividende, precision)}$` },
+      ]
+    })
+    return seb
+  }
+
+  /**
+   *
+   * @param nb1 diviseur (est écrit dans les cases du dessus)
+   * @param nb2 dividende (est écrit dans la case du dessous, mettre undefined pour avoir un ?)
+   * @param nbFois Le nombre de fois écrit sur l'accolade (un string qui peut être égal à '?')
+   * @param reste Le reste de la division (un string qui peut être égal à '?')
+   * @param precision
+   * @returns
+   */
+  static divisionAvecReste (nb1: number, nb2: number | undefined, nbFois:string, precision: number, reste?: string): SchemaEnBoite {
+    const seb = new SchemaEnBoite({
+      braceT: [
+        {
+          start: 1,
+          end: 8 + 2 * precision,
+          text: `\\text{${nbFois} fois}`,
+        }
+      ],
+      top: [
+        { color: 'lightgray', length: 2 + precision, content: `$${texNombre(nb1, precision)}$` },
+        { color: 'lightgray', length: 3, content: '...' },
+        { color: 'lightgray', length: 2 + precision, content: `$${texNombre(nb1, precision)}$` },
+        { color: 'lightgray', length: 2, content: reste ?? '?' }
+      ],
+      bottom: [
+        { color: 'white', length: 9 + 2 * precision, content: nb2 == null ? '?' : `$${texNombre(nb2, precision)}$` },
+      ]
+    })
+    return seb
+  }
+
+  /**
+ *
+ * @param nb1 Le diviseur (écrit dans la case du dessus
+ * @param nb2 Le dividende (écrit dans la case du dessous peut être unedfined pour avoir un ?)
+ * @param nb3 Le nombre de fois écrit sur l'accolade (un
+ * @param precision
+ * @param reste
+ * @returns
+ */
+  divisionPartitionAvecReste (nb1: number | undefined, nb2: number | undefined, nb3: number | undefined, precision: number, reste?: string): SchemaEnBoite {
+    const seb = new SchemaEnBoite({
+      braceT: [
+        {
+          start: 1,
+          end: 8 + 2 * precision,
+          text: nb3 == null ? '?' : `\\text{${texNombre(nb3, 0)} fois}`,
+        }
+      ],
+      top: [
+        { color: 'lightgray', length: 2 + precision, content: nb1 == null ? '?' : `$${texNombre(nb1, precision)}$` },
+        { color: 'lightgray', length: 3, content: '...' },
+        { color: 'lightgray', length: 2 + precision, content: nb1 == null ? '?' : `$${texNombre(nb1, precision)}$` },
+        { color: 'lightgray', length: 2, content: reste ?? '?' }
+      ],
+      bottom: [
+        { color: 'white', length: 9 + 2 * precision, content: nb2 == null ? '?' : `$${texNombre(nb2, precision)}$` },
+      ]
+    })
+    return seb
+  }
+
+  static addition (nb1: number, nb2: number, precision: number): SchemaEnBoite {
+    const seb = new SchemaEnBoite({
+      top: [
+        { color: 'lightgray', length: 2 + precision, content: `$${texNombre(nb1, precision)}$` },
+        { color: 'lightgray', length: 2 + precision, content: `$${texNombre(nb2, precision)}$` },
+      ],
+      bottom: [
+        { color: 'white', length: 4 + precision * 2, content: '?' },
+      ]
+    })
+    return seb
+  }
+
+  static soustraction (terme1: number | undefined, difference: number | undefined, terme2: number | undefined, precision: number): SchemaEnBoite {
+    const seb = new SchemaEnBoite({
+      top: [
+        { color: 'lightgray', length: 2 + precision, content: terme2 == null ? '?' : `$${texNombre(terme2, precision)}$` },
+        { color: 'lightgray', length: 2 + precision, content: difference == null ? '?' : `$${texNombre(difference, precision)}$` },
+      ],
+      bottom: [
+        { color: 'white', length: 4 + precision * 2, content: terme1 == null ? '?' : `$${texNombre(terme1, precision)}$` },
+      ]
+    })
+    return seb
   }
 }
