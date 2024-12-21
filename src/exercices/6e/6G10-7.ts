c2ae2c32b68beafe2a0876be9f927d94833319e1
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2024-12-21 12:51:09 +0000

    JC-FixUuid35282

---
+++ src/exercices/6e/6G10-7.ts
@@ -0,0 +1,17 @@
+import VocabulaireDuCercle from './6G10-4'
+/**
+ * Clone de 6G10-4 pour rien (juste pour définir l'uuid 35282 qui a été diffusée et utilisée par Myriade)
+ */
+export { titre } from './6G10-4'
+export const uuid = '35282'
+// Ne pas référencer cet exo, il ferait doublon avec 6G10-4
+export const refs = {
+  'fr-fr': [],
+  'fr-ch': []
+}
+export default class LeCercle extends VocabulaireDuCercle {
+  constructor () {
+    super()
+    this.nbQuestions = 1
+  }
+}
