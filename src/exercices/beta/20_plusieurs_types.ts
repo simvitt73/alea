bf2c0537cea85eca1dc2fdb5c286786d01f60ec8
Rémi Angot <remi.angot@gmail.com>
2025-01-26 11:15:18 +0100

    Mise à jour du modèle pour évitrer erreur

--- src/exercices/beta/20_plusieurs_types.ts
+++ src/exercices/beta/20_plusieurs_types.ts
@@ -1,11 +1,12 @@
 import Exercice from '../Exercice'
 import { combinaisonListes } from '../../lib/outils/arrayOutils'
-import { listeQuestionsToContenu } from '../../modules/outils'
+import { listeQuestionsToContenu, randint } from '../../modules/outils'
 export const titre = 'Nom de l\'exercice'
 
 export const dateDePublication = '4/5/2024' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
 
-export const uuid = '0e694' // Quand on exécute pnpm start la première fois, le terminal renvoie une référence d'uuid, à copier-coller ici
+export const uuid = 'aModifier'
+// Quand on exécute pnpm start la première fois, le terminal renvoie une référence d'uuid, à copier-coller sur la ligne au-dessus
 export const refs = {
   'fr-fr': [],
   'fr-ch': []
@@ -16,7 +17,7 @@ export const refs = {
  * @author
 
 */
-export default class nomExercice extends Exercice {
+export default class NomExercice extends Exercice {
   constructor () {
     super()
     this.consigne = 'Consigne'
@@ -29,21 +30,22 @@ export default class nomExercice extends Exercice {
     for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50;) {
       let texte = ''
       let texteCorr = ''
+      const a = randint(-10, 10)
       switch (listeTypeQuestions[i]) {
         case 'type1':
-          texte = `Question ${i + 1} de type 1`
+          texte = `Question ${i + 1} de type 1 avec ${a}`
           texteCorr = `Correction ${i + 1} de type 1`
           break
         case 'type2':
           texte = `Question ${i + 1} de type 2`
           texteCorr = `Correction ${i + 1} de type 2`
           break
-        case 'type3':
+        default: // On termine toujours par default qui ici correspond à 'type3':
           texte = `Question ${i + 1} de type 3`
           texteCorr = `Correction ${i + 1} de type 3`
           break
       }
-      if (this.questionJamaisPosee(i, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
+      if (this.questionJamaisPosee(i, a)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
         this.listeQuestions[i] = texte
         this.listeCorrections[i] = texteCorr
         i++
