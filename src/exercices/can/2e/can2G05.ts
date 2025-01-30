df5eae861f2ab4d72fcb7a3b4d2684c7835b8315
moragi <gilles.mora@ac-normandie.fr>
2025-01-30 09:52:38 +0100

    correction sur can2G05

--- src/exercices/can/2e/can2G05.ts
+++ src/exercices/can/2e/can2G05.ts
@@ -46,27 +46,19 @@ export default class DistanceRepere extends Exercice {
           \\sqrt{(${c}-${ecritureParentheseSiNegatif(a)})^2+(${d}-${ecritureParentheseSiNegatif(b)})^2}=
           \\sqrt{${(c - a) ** 2}+${(d - b) ** 2}}=\\sqrt{${(c - a) ** 2 + (d - b) ** 2}}=
           ${miseEnEvidence(`${Math.sqrt((c - a) ** 2 + (d - b) ** 2)}`)}$<br><br>`
-          this.correction += texteEnCouleur(` Mentalement : <br>
-          On calcule  $(${c}-${a})^2$ et $(${d}-${b})^2$, ce qui donne $${(c - a) ** 2}$ et $${(d - b) ** 2}$. <br>
-          Puis on calcule la somme de ces nombres soit $${(c - a) ** 2}+${(d - b) ** 2}=${(c - a) ** 2 + (d - b) ** 2}$.<br>
-           Enfin, on en prend la racine carrée.`, 'blue')
-          this.reponse = Math.sqrt((c - a) ** 2 + (d - b) ** 2)
         } else {
           this.question = `Dans un repère du plan d'origine $O$, on donne $${nom[0]}(${a}\\,;\\,${b})$ et $${nom[1]}(${c}\\,;\\,${d})$.<br>
         Déterminer la longueur du segment $[${nom[0]}${nom[1]}]$.`
-
           this.correction = `$${nom[0]}${nom[1]}=
           \\sqrt{(x_${nom[1]}-x_${nom[0]})^2+(y_${nom[1]}-y_${nom[0]})^2}=
           \\sqrt{(${c}-${ecritureParentheseSiNegatif(a)})^2+(${d}-${ecritureParentheseSiNegatif(b)})^2}=
           \\sqrt{${(c - a) ** 2}+${(d - b) ** 2}}=${miseEnEvidence(`\\sqrt{${(c - a) ** 2 + (d - b) ** 2}}`)}$<br><br>`
-          this.correction += texteEnCouleur(` Mentalement : <br>
+        }
+        this.correction += texteEnCouleur(` Mentalement : <br>
           On calcule  $(${c}-${a})^2$ et $(${d}-${b})^2$, ce qui donne $${(c - a) ** 2}$ et $${(d - b) ** 2}$. <br>
           Puis on calcule la somme de ces nombres soit $${(c - a) ** 2}+${(d - b) ** 2}=${(c - a) ** 2 + (d - b) ** 2}$.<br>
-           Enfin, on en prend la racine carrée.<br>
-            Comme $${(c - a) ** 2 + (d - b) ** 2}$ n'est pas un carré parfait, on ne simplifie pas (la réponse doit être sous la forme $\\sqrt{a}$ ou d'un entier).
-           `, 'blue')
-          this.reponse = [`\\sqrt{${(c - a) ** 2 + (d - b) ** 2}}`, `${Math.sqrt((c - a) ** 2 + (d - b) ** 2)}`]
-        }
+           Enfin, on en prend la racine carrée.`, 'blue')
+        this.reponse = `\\sqrt{${(c - a) ** 2 + (d - b) ** 2}}`
         if (this.interactif) { this.question += `<br>$${nom[0]}${nom[1]}=$` }
         this.canEnonce = this.question// 'Compléter'
         this.canReponseACompleter = `$${nom[0]}${nom[1]}=\\ldots$`
@@ -87,7 +79,6 @@ export default class DistanceRepere extends Exercice {
          Enfin, on en prend la racine carrée.<br>
           Comme $25$ est pas un carré parfait, on  simplifie.
          `, 'blue')
-          this.reponse = Math.sqrt((a) ** 2 + (b) ** 2)
         } else {
           this.question = `Dans un repère du plan d'origine $O$, on donne $${nom[0]}(${a}\\,;\\,${b})$.<br>
         Déterminer la longueur du segment $[O${nom[0]}]$.`
@@ -99,8 +90,8 @@ export default class DistanceRepere extends Exercice {
         Puis on calcule la somme de ces nombres soit $${a ** 2}+${b ** 2}=${a ** 2 + b ** 2}$.<br>
          Enfin, on en prend la racine carrée.         
          `, 'blue')
-          this.reponse = `\\sqrt{${a ** 2 + b ** 2}}`
           if (this.interactif) { this.question += `<br>$O${nom[0]}=$` }
+          this.reponse = `\\sqrt{${(a) ** 2 + (b) ** 2}}`
         }
 
         this.canEnonce = this.question// 'Compléter'
