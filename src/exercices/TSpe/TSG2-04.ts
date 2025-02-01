1d74b1be3c551c7b47f3569dbcba67263eb4425e
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2025-02-01 12:53:48 +0100

    Modification du code : Les coordonnées sont choisies.

--- src/exercices/TSpe/TSG2-04.ts
+++ src/exercices/TSpe/TSG2-04.ts
@@ -1,8 +1,7 @@
 import Exercice from '../Exercice'
-import { combinaisonListes } from '../../lib/outils/arrayOutils'
+import { combinaisonListes, shuffle2tableaux, shuffle3tableaux } from '../../lib/outils/arrayOutils'
 import { listeQuestionsToContenu, randint } from '../../modules/outils'
 import { ecritureAlgebrique, ecritureAlgebriqueSauf1, ecritureParentheseSiNegatif } from '../../lib/outils/ecritures'
-import FractionEtendue from '../../modules/FractionEtendue'
 export const titre = 'Déterminer deux droites sont orthogonales.'
 
 export const dateDePublication = '29/01/2025' // La date de publication initiale au format 'jj/mm/aaaa' pour affichage temporaire d'un tag
@@ -30,24 +29,46 @@ export default class nomExercice extends Exercice {
 
     const listeTypeQuestions = combinaisonListes(typeQuestionsDisponibles, this.nbQuestions) // Tous les types de questions sont posés mais l'ordre diffère à chaque "cycle"
     for (let i = 0, texte, texteCorr, cpt = 0; i < this.nbQuestions && cpt < 50;) { // Boucle principale où i+1 correspond au numéro de la question
-      const xA = randint(-10, 10, 0)
-      const yA = randint(-10, 10, 0)
-      const zA = randint(-10, 10, 0)
-      const xB = randint(-10, 10, [0, xA])
-      const yB = randint(-10, 10, [0, yA])
-      let zB = 0
+      let xA = randint(-10, 10, 0)
+      let yA = randint(-10, 10, 0)
+      let zA = randint(-10, 10, 0)
+      let yB = randint(-10, 10, [0, yA])
+      const deltaZ = randint(-3, 3, [0])
+      let zB = zA + deltaZ // On s'arrange pour que zB-zA = -2, -1, 1 ou 2.
+      const k = randint(-2, 2, [0]) // ce qui donne (xAB/zAB = k)
+      let xB = xA + k * (zB - zA)
+      const xAB = xB - xA
+      const yAB = yB - yA
+      const zAB = zB - zA
+      // on doit trouver a, b et c tels que a*xAB+b*yAB+c*zAB=0
+      // on choisit arbitrairement a=- 1 ou a = 1
+      // on prend b=0
+      // on trouve c=-x/z
+      let a = randint(-3, 3, [0])
+      let b = 0
+      let c = -a * k // il faut que ce nombre soit entier... On définit xAB et zAB de manière à ce que ce soit le cas
+      const t = randint(-4, 4, [0, 1])
+
       const x = randint(-10, 10, [0, xA, xB])
       const y = randint(-10, 10, [0, yA, yB])
       const z = randint(-10, 10, [0, zA])
-      const a = randint(-10, 10, 0)
-      const b = randint(-10, 10, 0)
-      const c = randint(-10, 10, 0)
-      const t = randint(-4, 4, [0, 1])
-      const alea = randint(1, 3)// aléatoirisation pour non appartenance, déterminer quelle cooronnée est modifiée pour planter
+
+      const tab1 = [a, b, c].slice()
+      const tab2 = [xA, yA, zA].slice()
+      const tab3 = [xB, yB, zB].slice()
+      shuffle3tableaux(tab1, tab2, tab3)
+      a = tab1[0]
+      b = tab1[1]
+      c = tab1[2]
+      xA = tab2[0]
+      yA = tab2[1]
+      zA = tab2[2]
+      xB = tab3[0]
+      yB = tab3[1]
+      zB = tab3[2]
+
       switch (listeTypeQuestions[i]) { // Suivant le type de question, le contenu sera différent
         case 'type1':// orthogonal
-          zB = a * xA + b * yA + c * zA - b * yB - a * xA
-          zB=
           break
         case 'type2':// défavorable
         default :
@@ -56,7 +77,7 @@ export default class nomExercice extends Exercice {
       }
       texte = 'Dans un repère orthonormé de l\'espace, on donne les coordonénes des points $A$ et $B$ et la représentation paramétrique d\'une droite $(\\Delta)$ :<br>'
       texte += `$ A(${xA}~;~${yA}~;~${zA})$ ; $B(${xB}~;~${yB}~;~${zB})$ et `
-      texte += `$(\\Delta) : \\begin{cases}x=${x}${ecritureAlgebriqueSauf1(a)} t\\\\y=${y}${ecritureAlgebriqueSauf1(b)} t\\quad (t\\in\\mathbb{R})\\\\z=${z}${ecritureAlgebriqueSauf1(c)} t\\end{cases}$`
+      texte += `$(\\Delta) : \\begin{cases}x=${x}${a !== 0 ? `${ecritureAlgebriqueSauf1(a)}t` : ''}\\\\y=${y}${b !== 0 ? `${ecritureAlgebriqueSauf1(b)}t` : ''} \\quad (t\\in\\mathbb{R})\\\\z=${z}${c !== 0 ? `${ecritureAlgebriqueSauf1(c)}t` : ''}\\end{cases}$`
       texte += 'Déterminer si les doites $(AB)$ et $\\Delta)$ sont orthogonales.'
       texteCorr = 'Pour prouver l\'orthogonalité de deux droites, il suffit de vérifier si le produit scalaire de deux de leurs vecteurs directeurs respectifs est nul.<br>'
       texteCorr += 'Un vecteur directeur de la droite $(\\Delta)$ est, d\'après le cours, directement donné par sa représentation paramétrique :<br>'
@@ -65,7 +86,7 @@ export default class nomExercice extends Exercice {
       texteCorr += `On en déduit que dans notre situation, un vecteur directeur de la droite $(\\Delta)$ est $\\overrightarrow{u}\\begin{pmatrix} ${a}\\\\${b}\\\\${c}\\end{pmatrix}$<br>`
       texteCorr += 'On calcule les coordonnées du vecteur $\\overrightarrow{AB}$ vecteur directeur de la droite $(AB)$ :<br>'
       texteCorr += `$\\overrightarrow{AB}\\begin{pmatrix} ${xB}${ecritureAlgebrique(xA)}\\\\${yB}${ecritureAlgebrique(yA)}\\\\${zB}${ecritureAlgebrique(zA)}\\end{pmatrix}$<br>`
-      texteCorr += `$\\iff\\overrightarrow{AB}\\begin{pmatrix} ${xB - xA}\\\\${yB - yA}\\\\${zB - zA}\\end{pmatrix}$<br>`
+      texteCorr += `$\\iff\\overrightarrow{AB}\\begin{pmatrix} ${xAB}\\\\${yAB}\\\\${zAB}\\end{pmatrix}$<br>`
       texteCorr += 'On calcule le produit scalaire de ces deux vecteurs : <br>'
       texteCorr += `$\\overrightarrow{u}\\cdot\\overrightarrow{AB} = ${a}\\times ${ecritureParentheseSiNegatif(xB - xA)}+${ecritureParentheseSiNegatif(b)}\\times ${ecritureParentheseSiNegatif(yB - yA)}+${ecritureParentheseSiNegatif(c)}\\times ${ecritureParentheseSiNegatif(zB - zA)}=${a * (xB - xA) + b * (yB - yA) + c * (zB - zA)}$<br>`
       if (this.questionJamaisPosee(i, a, b, c, xA, yA, zA, xB, yB, zB, x, y, z, t, texte)) { // <- laisser le i et ajouter toutes les variables qui rendent les exercices différents (par exemple a, b, c et d)
