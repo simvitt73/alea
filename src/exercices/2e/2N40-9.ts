c466f14d5fae5484d67c5fb168ae517703d729f0
gillesMora <gilles.mora@ac-normandie.fr>
2025-02-20 19:33:06 +0100

    feat: clone exercices pour secondes

---
+++ src/exercices/2e/2N40-9.ts
@@ -0,0 +1,13 @@
+import FactoriserParNombreOux from '../3e/3L11-4'
+export const titre = 'Factoriser une expression'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCOpen'
+export const uuid = '2e5df'
+export const refs = {
+  'fr-fr': ['2N40-9'],
+  'fr-ch': []
+}
+export default class FactoriserParNombreOux2e extends FactoriserParNombreOux {
+}
