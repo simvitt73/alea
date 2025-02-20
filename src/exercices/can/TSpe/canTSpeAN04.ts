ad22b3e57dc94326089ded583b27f8c40d53a051
mathsguyon <mathsguyon@gmail.com>
2025-02-20 15:00:28 +0100

    Create canTSpeAN04.ts

---
+++ src/exercices/can/TSpe/canTSpeAN04.ts
@@ -0,0 +1,46 @@
+import Exercice from '../../Exercice'
+import { randint } from '../../../modules/outils'
+import { ecritureAlgebriqueSauf1, ecritureParentheseSiNegatif, rienSi1 } from '../../../lib/outils/ecritures'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+export const titre = 'Somme élémentaire avec le logarithme népérien'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+
+export const dateDePublication = '20/02/2025'
+
+export const uuid = '7c230' // Quand on exécute pnpm start la première fois, le terminal renvoie une référence d'uuid, à copier-coller ici
+export const refs = {
+  'fr-fr': ['canTSpeAN04'],
+  'fr-ch': []
+}
+
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Stéphane Guyon
+
+*/
+export default class logarithme extends Exercice {
+  constructor () {
+    super()
+    this.typeExercice = 'simple'
+    this.nbQuestions = 1
+  }
+
+  nouvelleVersion () {
+    const a1 = randint(-5, 5, 0)
+    const a2 = randint(-5, 5, 0)
+    const a3 = randint(-5, 5, 0)
+    const n1 = randint(-3, 3, [0, 1])
+    const n3 = randint(-3, 3, [0, 1])
+    const a4 = randint(-5, 5, 0)
+    const n4 = randint(-3, 3, [0, 1])
+    this.question = `Calculer $A=${rienSi1(a1)} \\ln\\left(\\text{e}^{${n1}}\\right)${ecritureAlgebriqueSauf1(a2)} \\ln(1)${ecritureAlgebriqueSauf1(a3)} \\ln\\left(\\dfrac{1}{\\text{e}^{${n3}}}\\right)${ecritureAlgebriqueSauf1(a4)}\\ln\\left(\\text{e}^{${n4}}\\right)$.`
+    this.correction = `$\\begin{aligned} A&=${rienSi1(a1)} \\ln\\left(\\text{e}^{${n1}}\\right)${ecritureAlgebriqueSauf1(a2)} \\ln(1)${ecritureAlgebriqueSauf1(a3)} \\ln\\left(\\dfrac{1}{\\text{e}^{${n3}}}\\right)${ecritureAlgebriqueSauf1(a4)}\\ln\\left(\\text{e}^{${n4}}\\right)\\\\
+    &=${a1} \\times ${ecritureParentheseSiNegatif(n1)}\\ln(\\text{e})${ecritureAlgebriqueSauf1(a2)} \\times 0${ecritureAlgebriqueSauf1(a3)} \\times ${ecritureParentheseSiNegatif(n3)}\\ln\\left(\\dfrac{1}{\\text{e}}\\right)${ecritureAlgebriqueSauf1(a4)}\\times ${ecritureParentheseSiNegatif(n4)}\\\\
+    &=${a1 * n1} \\times 1 ${ecritureAlgebriqueSauf1(a3 * n3)} \\times \\ln\\left({\\text{e}}^{-1}\\right)${ecritureAlgebriqueSauf1(a4 * n4)}\\\\
+    &=${a1 * n1}${ecritureAlgebriqueSauf1(-a3 * n3)}${ecritureAlgebriqueSauf1(a4 * n4)}\\\\
+    &=${miseEnEvidence(a1 * n1 - a3 * n3 + a4 * n4)}\\end{aligned}$`
+    this.optionsChampTexte = { texteAvant: '<br>$A=~$' }
+    this.reponse = a1 * n1 - a3 * n3 + a4 * n4
+  }
+}
