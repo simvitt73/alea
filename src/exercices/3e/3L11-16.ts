06ea88377637a8cadfe622590a02338ae6f46e7d
Nathan <nathan.scheinmann@edu.ge.ch>
2025-03-14 08:06:51 +0100

    petit changement mais je ne comprends toujours pas l'erreur

--- src/exercices/3e/3L11-16.ts
+++ src/exercices/3e/3L11-16.ts
@@ -26,13 +26,13 @@ export default class nomExercice extends Exercice {
     super()
     this.consigne = 'Compléter le terme manquant afin d\'obtenir une identité remarquable. Écrire ensuite l\'identité remarquable correspondante factorisée.'
     this.nbQuestions = 4
-    this.besoinFormulaireTexte = ['Choix des questions', 'Nombres séparés par des tirets\n1 - (x+a)^2\n2 - (x-a)^2\n3 - (x-a)(x-b)\n4 - (x+a)(x+b)\n5 - (ax+by)^2\n6 - (ax-by)^2\n7 - (ax+by)(ax-by)\n8 - (ax+b)(ax+c)\n9 - Mélange']
+    this.besoinFormulaireTexte = ['Choix des questions', 'Nombres séparés par des tirets\n1 - (x+a)^2\n2 - (x-a)^2\n3 - (x+a)(x+b)\n4 - (ax+by)^2\n5 - (ax-by)^2\n6 - (ax+b)(ax+c)\n7 - Mélange']
     this.besoinFormulaire2Numerique = ['Coefficients', 3, 'Entiers \n2 : Fractionnaires \n3 : Mélange']
     this.besoinFormulaire3Numerique = ['Degré maximum', 5, '1\n2\n3\n4\n5']
     this.besoinFormulaire4Numerique = ['Nombre de variables différentes', 5, '1\n2\n3\n4\n5']
     // this.besoinFormulaireCaseACocher = ['Type de coefficients', 3, 'Entiers\nFractionnaires\nMélange']
     // this.besoinFormulaire5Numerique = ['Nombre de termes', 5, '1\n2\n3\n4\n5']
-    this.sup = 9
+    this.sup = 7
     this.sup2 = 1
     this.sup3 = 2
     this.sup4 = 2
@@ -45,9 +45,9 @@ export default class nomExercice extends Exercice {
     const listeDeQuestions = gestionnaireFormulaireTexte({
       saisie: this.sup,
       min: 1,
-      max: 8,
-      melange: 9,
-      defaut: 9,
+      max: 6,
+      melange: 7,
+      defaut: 7,
       nbQuestions: this.nbQuestions
 
     })
@@ -74,7 +74,7 @@ export default class nomExercice extends Exercice {
       } else {
         typeofCoeff = typeCoeffListe
       }
-      if (listeDeQuestions[i] === 1 || listeDeQuestions[i] === 2 || listeDeQuestions[i] === 3 || listeDeQuestions[i] === 4) {
+      if (listeDeQuestions[i] === 1 || listeDeQuestions[i] === 2 || listeDeQuestions[i] === 3) {
         p1 = MonomePlusieursVariables.createRandomMonome(degMax, choice(typeofCoeff), variablesSelect)
         p2 = MonomePlusieursVariables.createRandomMonome(0, choice(typeofCoeff), variablesSelect)
         pSP1 = MonomePlusieursVariables.createRandomMonome(0, 'entier', variablesSelect)
@@ -86,7 +86,7 @@ export default class nomExercice extends Exercice {
           p2 = p2.oppose()
         }
       }
-      if (listeDeQuestions[i] === 5 || listeDeQuestions[i] === 6 || listeDeQuestions[i] === 7 || listeDeQuestions[i] === 8) {
+      if (listeDeQuestions[i] === 4 || listeDeQuestions[i] === 5 || listeDeQuestions[i] === 6) {
         p1 = MonomePlusieursVariables.createRandomMonome(randint(1, max(1, degMax)), choice(typeofCoeff), variablesSelect)
         do {
           p2 = MonomePlusieursVariables.createRandomMonome(randint(1, max(1, degMax)), choice(typeofCoeff), variablesSelect)
@@ -106,7 +106,7 @@ export default class nomExercice extends Exercice {
       }
       switch (listeDeQuestions[i]) {
         case 1:
-        case 5:
+        case 4:
         {
           const expr = IdentiteRemarquable.carreDuneSomme(p1, p2).melangerTermes(true)
           texte = `$${lettreDepuisChiffre(i + 1)}=${expr.toStringSansLeDernierTerme()}\\,=\\,\\ldots\\ldots$`
@@ -114,7 +114,7 @@ export default class nomExercice extends Exercice {
           break
         }
         case 2:
-        case 6:
+        case 5:
         {
           const expr = IdentiteRemarquable.carreDuneDifference(p1, p2).melangerTermes(true)
           texte = `$${lettreDepuisChiffre(i + 1)}=${expr.toStringSansLeDernierTerme()}\\,=\\,\\ldots\\ldots$`
@@ -122,17 +122,7 @@ export default class nomExercice extends Exercice {
           break
         }
         case 3:
-        case 7:
-        {
-          const signe = ['+', '-']
-          const choixSigne = randint(0, 1)
-          const expr = IdentiteRemarquable.differenceDeDeuxCarres(p1, p2).melangerTermes(true)
-          texte = `$${lettreDepuisChiffre(i + 1)}=${expr.toStringSansLeDernierTerme()}\\,=\\,\\ldots\\ldots$`
-          texteCorr = `$${lettreDepuisChiffre(i + 1)}=${expr.toStringAvecDernierTermeEnEvidence()}=${miseEnEvidence(`\\left(${p1.toString()}${signe[choixSigne]} ${p2.toString()}\\right)\\left(${p1.toString()}${signe[(choixSigne + 1) % 2]}${p2.toString()}\\right)`)}$`
-          break
-        }
-        case 4:
-        case 8:
+        case 6:
         {
           const signeT1 = pSP1.coefficient.signe < 0 ? '' : '+'
           const signeT2 = pSP2.coefficient.signe < 0 ? '' : '+'
