a0ce7eec877d6159bc2298f6cb0328ce09985813
Cristobal-84 <76251099+Cristobal-84@users.noreply.github.com>
2025-03-30 16:31:42 +0200

    ex : Ajout 2G22-2 et divers modifs

---
+++ src/exercices/2e/2G22-2.ts
@@ -0,0 +1,25 @@
+import LectureGraphiqueVecteurRepere from '../can/2e/can2G18'
+
+export const titre = 'Lire les coordonnées d’un vecteur représenté dans un repère'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+
+export const dateDePublication = '29/03/2025'
+
+export const uuid = 'a02bc'
+export const refs = {
+  'fr-fr': ['2G22-2'],
+  'fr-ch': []
+}
+
+/**
+ * Lire les coordonnées d'un vecteur représenté dans un repère.
+ * @author Stéphan Grignon
+ */
+
+export default class LectureGraphiqueVecteurRepere2nde extends LectureGraphiqueVecteurRepere {
+  constructor () {
+    super()
+    this.nbQuestions = 1
+  }
+}
