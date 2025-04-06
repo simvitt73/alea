feb80da8adc0c74cfc6fba0e48d7a17470a4e63e
Rémi Angot <remi.angot@gmail.com>
2025-04-06 10:57:53 +0200

    wip

---
+++ src/exercicesInteractifs/QuestionsDeCours.svelte
@@ -0,0 +1,61 @@
+<script>
+    import Exercice from '../exercices/Exercice'
+    import HeaderExerciceVueProf from '../components/shared/exercice/shared/headerExerciceVueProf/HeaderExerciceVueProf.svelte'
+    import { ajouteChampTexteMathLive } from '../lib/interactif/questionMathLive'
+    import { loadMathLive } from '../modules/loaders'
+    import { afterUpdate } from 'svelte'
+    import { KeyboardType, convertToKeyboardCategory, convertKeyboardTypeToBlocks } from '../lib/interactif/claviers/keyboard'
+
+    export let indiceExercice
+    export let indiceLastExercice
+
+    export const titre = 'Questions de cours'
+   // Cette ressource n'a pas d'uuid ou de ref
+   // Son accès est défini dans uuidsRessources.json
+
+    const headerExerciceProps = {
+      title: '',
+      isInteractif: false,
+      settingsReady: false,
+      interactifReady: false,
+      randomReady: false,
+      correctionReady: false
+    }
+
+    const exercice = new Exercice()
+    exercice.id = 'questionsDeCours'
+    exercice.numeroExercice = indiceExercice
+    exercice.interactif = true
+    const claviers = Object.values(KeyboardType)
+    let clavierSelected = KeyboardType.clavierFullOperations
+
+    afterUpdate(() => {
+      loadMathLive()
+    })
+  </script>
+
+  <HeaderExerciceVueProf
+    {indiceExercice}
+    {indiceLastExercice}
+    id="clavier"
+    {...headerExerciceProps}
+  />
+
+  <br>
+  Les types de clavier:
+  <select bind:value={clavierSelected}>
+      {#each claviers as clavier}
+          <option value={clavier}>
+              {clavier}
+          </option>
+      {/each}
+  </select>
+
+  <br>Nombre de blocs de touches :  {convertKeyboardTypeToBlocks(convertToKeyboardCategory(clavierSelected)).length}
+  <br>Liste de blocs : {convertKeyboardTypeToBlocks(convertToKeyboardCategory(clavierSelected)).join('; ')}
+
+  <br><br>
+  Exemple:
+  <br><br><pre>ajouteChampTexteMathLive(exercice, i, KeyboardType.{clavierSelected}) </pre>
+  <br><br>Et le résultat:
+  <div>{@html ajouteChampTexteMathLive(exercice, 0, convertToKeyboardCategory(clavierSelected))}</div>
