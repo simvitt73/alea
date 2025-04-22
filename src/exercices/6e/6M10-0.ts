8e3dcc3c818ba89e3a8ca23d2e8d2ba9e65932af
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2025-04-22 21:15:42 +0000

    3 m10 0 plus perimetre

--- src/exercices/6e/6M10-0.ts
+++ src/exercices/6e/6M10-0.ts
@@ -1,13 +1,16 @@
-import { Tetris } from '../../lib/2d/polygones'
+import { point } from '../../lib/2d/points'
+import { carre, Tetris } from '../../lib/2d/polygones'
 import { grille } from '../../lib/2d/reperes'
+import { segment } from '../../lib/2d/segmentsVecteurs'
 import { texteParPosition } from '../../lib/2d/textes'
 import { ajouteQuestionMathlive } from '../../lib/interactif/questionMathLive'
 import { miseEnEvidence } from '../../lib/outils/embellissements'
-import { mathalea2d } from '../../modules/2dGeneralites'
+import { sp } from '../../lib/outils/outilString'
+import { colorToLatexOrHTML, mathalea2d } from '../../modules/2dGeneralites'
 import { gestionnaireFormulaireTexte, randint } from '../../modules/outils'
 import Exercice from '../Exercice'
 
-export const titre = 'Trouver une aire par comptage'
+export const titre = 'Trouver une aire ou un périmètre par comptage'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 
@@ -26,7 +29,9 @@ export default class AireParComptage extends Exercice {
   constructor () {
     super()
     this.besoinFormulaireTexte = ['Aire maximale', 'Nombres séparés par des tirets\n1 : Inférieure à 10\n2 : Inférieure à 20\n3 : Inférieure à 30\n4 : Mélange']
+    this.besoinFormulaire2Numerique = ['Mesures demandées', 3, '1 : Aire\n2 : Périmètre\n3 : Aire et périmètre']
     this.sup = '4'
+    this.sup2 = 1
   }
 
   nouvelleVersion () {
@@ -47,17 +52,42 @@ export default class AireParComptage extends Exercice {
       const xmin = tetris.rectangle.xMin - 1
       const ymin = tetris.rectangle.yMin - 1
       const decalage = tetris.rectangle.xMax + 1
-      const xmax = tetris2.rectangle.xMax + decalage + 2
+      const xmax = tetris2.rectangle.xMax + decalage + 4
       const ymax = Math.max(tetris.rectangle.yMax, tetris2.rectangle.yMax) + 1
       const fig1 = texteParPosition('Figure 1', (tetris.rectangle.xMin + tetris.rectangle.xMax) / 2, tetris.rectangle.yMin - 0.5)
       const fig2 = texteParPosition('Figure 2', (tetris2.rectangle.xMin + tetris2.rectangle.xMax) / 2 + decalage, tetris2.rectangle.yMin - 0.5)
-      const objets = [grille(xmin, ymin, xmax, ymax), tetris.poly, tetris2.poly, fig1, fig2]
+      const uniteAire = carre(point(xmax - 2, ymax - 2), point(xmax - 1, ymax - 2))
+      uniteAire.couleurDeRemplissage = colorToLatexOrHTML('gray')
+      const uniteLongueur = segment(point(xmax - 2, ymax - 4), point(xmax - 1, ymax - 4))
+      uniteLongueur.epaisseur = 2
+      uniteLongueur.styleExtremites = '|-|'
+      const texteUniteAire = texteParPosition('u.a', xmax - 1.5, ymax - 2.5)
+      const texteUniteLongueur = texteParPosition('u.l', xmax - 1.5, ymax - 4.5)
+
+      const objets = [grille(xmin, ymin, xmax, ymax), tetris.poly, tetris2.poly, fig1, fig2, uniteAire, uniteLongueur, texteUniteAire, texteUniteLongueur]
       const figure = mathalea2d(Object.assign({ pixelsParCm: 20, scale: 0.5 }, { xmin, ymin, xmax, ymax }), objets)
-      const texte = `${figure}<br><br>
-Quelle sont les aires des figures ci-dessus (l'unité d'aire est le carreau) ?<br>
- ${ajouteQuestionMathlive({ exercice: this, question: 2 * i, typeInteractivite: 'mathlive', texteAvant: 'figure 1 : ', texteApres: ' unités.  ', objetReponse: { reponse: { value: aire } } })}
- ${ajouteQuestionMathlive({ exercice: this, question: 2 * i + 1, typeInteractivite: 'mathlive', texteAvant: 'figure 2 : ', texteApres: ' unités.  ', objetReponse: { reponse: { value: aire2 } } })}`
-      const texteCorr = `L'aire de la figure est $${miseEnEvidence(aire)}$ unités.`
+      const texte = this.sup2 === 1
+        ? `${figure}<br><br>
+Quelles sont les aires des figures ci-dessus ?<br>
+ ${ajouteQuestionMathlive({ exercice: this, question: 2 * i, typeInteractivite: 'mathlive', texteAvant: 'figure 1 : ', texteApres: ` u.a ${sp(17)} `, objetReponse: { reponse: { value: aire } } })}
+ ${ajouteQuestionMathlive({ exercice: this, question: 2 * i + 1, typeInteractivite: 'mathlive', texteAvant: 'figure 2 : ', texteApres: ' u.a.', objetReponse: { reponse: { value: aire2 } } })}`
+        : this.sup2 === 2
+          ? `${figure}<br><br>
+ Quels sont les périmètres des figures ci-dessus ?<br>
+  ${ajouteQuestionMathlive({ exercice: this, question: 2 * i, typeInteractivite: 'mathlive', texteAvant: 'figure 1 : ', texteApres: ` u.l ${sp(6)}`, objetReponse: { reponse: { value: tetris.poly.perimetre } } })}
+  ${ajouteQuestionMathlive({ exercice: this, question: 2 * i + 1, typeInteractivite: 'mathlive', texteAvant: 'figure 2 : ', texteApres: ' u.l.', objetReponse: { reponse: { value: tetris2.poly.perimetre } } })}`
+          : `${figure}<br><br>
+  Quels sont les aires et les périmètres des figures ci-dessus ?<br>
+  ${ajouteQuestionMathlive({ exercice: this, question: 4 * i, typeInteractivite: 'mathlive', texteAvant: ' Aire figure 1 : ', texteApres: `u.a ${sp(17)}`, objetReponse: { reponse: { value: aire } } })}
+  ${ajouteQuestionMathlive({ exercice: this, question: 4 * i + 1, typeInteractivite: 'mathlive', texteAvant: 'Aire figure 2 : ', texteApres: 'u.a.', objetReponse: { reponse: { value: aire2 } } })}${this.interactif ? '<br>' : ''}
+  ${ajouteQuestionMathlive({ exercice: this, question: 4 * i + 2, typeInteractivite: 'mathlive', texteAvant: 'Périmètre figure 1 : ', texteApres: `u.l ${sp(6)}`, objetReponse: { reponse: { value: tetris.poly.perimetre } } })}
+  ${ajouteQuestionMathlive({ exercice: this, question: 4 * i + 3, typeInteractivite: 'mathlive', texteAvant: 'Périmètre figure 2 : ', texteApres: 'u.l.', objetReponse: { reponse: { value: tetris2.poly.perimetre } } })}`
+      const texteCorr = this.sup2 === 1
+        ? `L'aire de la figure 1 est $${miseEnEvidence(aire)}$ u.a et celle de la figure 2 est  $${miseEnEvidence(aire2)}$ u.a.`
+        : this.sup2 === 2
+          ? `Le périmètre de la figure 1 est $${miseEnEvidence(tetris.poly.perimetre)}$ u.l et celui de la figure 2 est $${miseEnEvidence(tetris2.poly.perimetre)}$ u.l.`
+          : `L'aire de la figure 1 est $${miseEnEvidence(aire)}$ u.a, celle de la figure 2 est $${miseEnEvidence(aire2)}$ u.a<br>
+          Le périmètre de la figure 1 est $${miseEnEvidence(tetris.poly.perimetre)}$ u.l et celui de la figure 2 est $${miseEnEvidence(tetris2.poly.perimetre)}$ u.l.`
       this.listeQuestions.push(texte)
       this.listeCorrections.push(texteCorr)
     }
