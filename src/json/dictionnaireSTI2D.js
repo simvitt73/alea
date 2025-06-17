dc4fca7b010d96db002aad0b770a47d065caf04c
Cristobal-84 <76251099+Cristobal-84@users.noreply.github.com>
2025-06-17 19:27:39 +0200

    URGENT: Tags sujet spé STI2D métro 06-2025

--- src/json/dictionnaireSTI2D.js
+++ src/json/dictionnaireSTI2D.js
@@ -1,5 +1,54 @@
 export const dictionnaireSTI2D = {
 
+  sti2d_2025_06_metropole_maths: {
+    annee: '2025',
+    lieu: 'Métropole',
+    mois: '06',
+    numeroInitial: 'maths',
+    typeExercice: 'sti2d',
+    tags: ['Nombres complexes', 'Géométrie repérée', 'Equations différentielles', 'Exponentielle', 'Dérivation', 'Equation', 'Logarithme népérien']
+  },
+  sti2d_2025_06_metropole_mq1: {
+    annee: '2025',
+    lieu: 'Métropole',
+    mois: '06',
+    numeroInitial: 'mq1',
+    typeExercice: 'sti2d',
+    tags: ['Nombres complexes', 'Géométrie repérée']
+  },
+  sti2d_2025_06_metropole_mq2: {
+    annee: '2025',
+    lieu: 'Métropole',
+    mois: '06',
+    numeroInitial: 'mq2',
+    typeExercice: 'sti2d',
+    tags: ['Equations différentielles', 'Exponentielle', 'Dérivation']
+  },
+  sti2d_2025_06_metropole_mq3: {
+    annee: '2025',
+    lieu: 'Métropole',
+    mois: '06',
+    numeroInitial: 'mq3',
+    typeExercice: 'sti2d',
+    tags: ['Exponentielle', 'Equation']
+  },
+  sti2d_2025_06_metropole_mq4: {
+    annee: '2025',
+    lieu: 'Métropole',
+    mois: '06',
+    numeroInitial: 'mq4',
+    typeExercice: 'sti2d',
+    tags: ['Logarithme népérien']
+  },
+  sti2d_2025_06_metropole_pcm: {
+    annee: '2025',
+    lieu: 'Métropole',
+    mois: '06',
+    numeroInitial: 'pcm',
+    typeExercice: 'sti2d',
+    tags: ['Dérivation', 'Trigonométrie', 'Intégration']
+  },
+
   sti2d_2024_06_metropole_maths: {
     annee: '2024',
     lieu: 'Métropole',
