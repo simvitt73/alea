d010008a04a647da6a5eb74145d5d065b00bd9ec
mathsguyon <mathsguyon@gmail.com>
2025-06-28 18:19:28 +0200

    maj

--- src/exercices/TSpe/TSG2-08.ts
+++ src/exercices/TSpe/TSG2-08.ts
@@ -63,7 +63,7 @@ export default class NomExercice extends Exercice {
           xB = alpha * ux + beta * vx + xA
           yB = alpha * uy + beta * vy + yA
           zB = alpha * uz + beta * vz + zA - 1  // Choisir zB pour que AB non coplanaires avec u et v
-          texte += `$(d):\\begin{cases}x=${rienSi0(xA)}  ${ecritureAlgebriqueSauf1(ux)}t\\\\y= ${rienSi0(yA)}  ${ecritureAlgebriqueSauf1(uy)}t\\quad(t\\in\\mathbb{R})\\\\z= ${rienSi0(zA)} ${ecritureAlgebriqueSauf1(uz)}t\\end{cases}$`
+          texte += `$(d):\\begin{cases}x=${reduireAxPlusB(ux, xA, 't', { ordreInverse: true })} \\\\y= ${rienSi0(yA)}  ${ecritureAlgebriqueSauf1(uy)}t\\quad(t\\in\\mathbb{R})\\\\z= ${rienSi0(zA)} ${ecritureAlgebriqueSauf1(uz)}t\\end{cases}$`
           texte += `$\\quad\\quad(d'):\\begin{cases}x=${rienSi0(xB)}${ecritureAlgebriqueSauf1(vx)}s\\\\y= ${rienSi0(yB)}  ${ecritureAlgebriqueSauf1(vy)}s\\quad(s\\in\\mathbb{R})\\\\z= ${rienSi0(zB)} ${ecritureAlgebriqueSauf1(vz)}s\\end{cases}$`
           texte += `<br><br>${texteEnCouleurEtGras('Affirmation :')}  ${affirmation}`
           texteCorr = 'On vérifie que les vecteurs directeurs des deux droites ne sont pas colinéaires :<br>'
