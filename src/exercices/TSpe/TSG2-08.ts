e57b3ff630f277faaa5604716bd37faf6ed2c150
mathsguyon <mathsguyon@gmail.com>
2025-06-29 21:40:51 +0200

    maj

--- src/exercices/TSpe/TSG2-08.ts
+++ src/exercices/TSpe/TSG2-08.ts
@@ -1,8 +1,7 @@
-/* eslint-disable no-case-declarations */
 import Exercice from '../Exercice'
 import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
 import { listeQuestionsToContenu, randint } from '../../modules/outils'
-import { ecritureAlgebriqueSauf1, reduireAxPlusB, rienSi0 } from '../../lib/outils/ecritures'
+import { ecritureAlgebrique, ecritureAlgebriqueSauf1, reduireAxPlusB, rienSi0, rienSi1 } from '../../lib/outils/ecritures'
 import { texteEnCouleurEtGras, texteItalique } from '../../lib/outils/embellissements'
 import FractionEtendue from '../../modules/FractionEtendue'
 
@@ -48,6 +47,9 @@ export default class NomExercice extends Exercice {
       let vx = randint(-10, 10, [ux, xA, 0])
       let vy = randint(-10, 10, [uy, yA, 0])
       let vz = randint(-10, 10, [uz, zA, 0])
+      let u1 = new FractionEtendue(vx, ux)
+      let u2 = new FractionEtendue(vy, uy)
+      let u3 = new FractionEtendue(vz, uz)
       const alpha = randint(-3, 3, 0) // Pour éviter les droites parallèles
       const beta = randint(-3, 3, 0) // Pour éviter les droites parallèles
       const affirmation = choice(['Les droites $(d)$ et $(d~\')$ sont parallèles.',
@@ -66,26 +68,57 @@ export default class NomExercice extends Exercice {
           xB = alpha * ux + beta * vx + xA
           yB = alpha * uy + beta * vy + yA
           zB = alpha * uz + beta * vz + zA - 1  // Choisir zB pour que AB non coplanaires avec u et v
-
-          const uxvx = new FractionEtendue(ux, vx)
-
-          const uyvy = new FractionEtendue(uy, vy)
-          const uzvz = new FractionEtendue(uz, vz)
-
+          u1 = new FractionEtendue(vx, ux)
+          u2 = new FractionEtendue(vy, uy)
+          u3 = new FractionEtendue(vz, uz)
           texte += `$(d):\\begin{cases}x=${reduireAxPlusB(ux, xA, 't', { ordreInverse: true })} \\\\y= ${rienSi0(yA)}  ${ecritureAlgebriqueSauf1(uy)}t\\quad(t\\in\\mathbb{R})\\\\z= ${rienSi0(zA)} ${ecritureAlgebriqueSauf1(uz)}t\\end{cases}$`
           texte += `$\\quad\\quad(d'):\\begin{cases}x=${rienSi0(xB)}${ecritureAlgebriqueSauf1(vx)}s\\\\y= ${rienSi0(yB)}  ${ecritureAlgebriqueSauf1(vy)}s\\quad(s\\in\\mathbb{R})\\\\z= ${rienSi0(zB)} ${ecritureAlgebriqueSauf1(vz)}s\\end{cases}$`
           texte += `<br><br>${texteEnCouleurEtGras('Affirmation :')}  ${affirmation}`
-          texteCorr = texteEnCouleurEtGras('Étape 1 : Colinéarité des vecteurs directeurs.')
+          texteCorr = texteEnCouleurEtGras('Étape 1 : Droites parallèles ?')
           texteCorr += '<br>On sait que la représentation paramétrique d\'une droite est de la forme '
           texteCorr += '$(d):\\begin{cases}x=x_A+at \\\\y= y_A+bt\\quad(t\\in\\mathbb{R})\\\\z= z_A+ct\\end{cases}$'
           texteCorr += '<br>où le point $A(x_A, y_A, z_A)$ est un point de la droite et $\\vec u\\begin{pmatrix}a\\\\b\\\\c\\end{pmatrix}$ est un vecteur directeur de la droite.<br>'
           texteCorr += `<br>On en déduit que les coordonnées de $\\vec u$ vecteur directeur de (d) sont $\\vec u\\begin{pmatrix} ${ux}\\\\${uy}\\\\${uz}\\end{pmatrix}$`
           texteCorr += `et celles de $\\vec v$ vecteur directeur de (d') sont $\\vec v\\begin{pmatrix} ${vx}\\\\${vy}\\\\${vz}\\end{pmatrix}$<br>`
           texteCorr += 'On compare pour les quotients des composantes de même dimension : <br>'
-          texteCorr += `$\\frac{${ux}}{${vx}} = ${uxvx}.texFraction$`
-          texteCorr += ` \\frac{${uy}}{${vy}} = \\frac{${uz}}{${vz}}$`
-          texteCorr += '<br>On trouve que les quotients ne sont pas égaux, donc les droites ne sont pas parallèles.<br>'
-          texteCorr += 'On vérifie que les droites ne sont pas sécantes'
+          texteCorr += `$\\frac{${ux}}{${vx}} = ${new FractionEtendue(ux, vx).texFractionSimplifiee}\\quad$`
+          texteCorr += `;$\\quad\\frac{${uy}}{${vy}} = ${new FractionEtendue(uy, vy).texFractionSimplifiee}\\quad$`
+          texteCorr += `;$\\quad\\frac{${uz}}{${vz}} = ${new FractionEtendue(uz, vz).texFractionSimplifiee}$`
+          texteCorr += '<br>On constate que les quotients ne sont pas égaux, donc les vecteurs ne sont donc pas colinéaires et les droites ne sont pas parallèles.<br><br>'
+          texteCorr += texteEnCouleurEtGras('Étape 2 : Droites sécantes ou non-coplanaires ?')
+          texteCorr += '<br>Si les droites $(d)$ et $(d\')$ sont sécantes, il existe un point d\'intersection $M(x;y;z)$ tel que $M\\in(d)\\cap (d\')$.'
+          texteCorr += '<br>Les coordonnées du point $M$ vérifient donc les deux représentations paramétriques. <br>Ce qui est équivalent à résoudre le système suivant :'
+          texteCorr += `<br>$\\begin{cases}
+            ${reduireAxPlusB(ux, xA, 't', { ordreInverse: true })} = ${reduireAxPlusB(vx, xB, 's', { ordreInverse: true })} \\\\
+            ${reduireAxPlusB(uy, yA, 't', { ordreInverse: true })} = ${reduireAxPlusB(vy, yB, 's', { ordreInverse: true })} \\\\
+            ${reduireAxPlusB(uz, zA, 't', { ordreInverse: true })} = ${reduireAxPlusB(vz, zB, 's', { ordreInverse: true })}
+            \\end{cases}$`
+          texteCorr += '<br>On peut réécrire ce système sous la forme :'
+          texteCorr += `<br>$\\begin{cases}
+            ${rienSi1(ux)}t = ${rienSi1(vx)}s ${ecritureAlgebrique(xB - xA)} \\\\
+            ${rienSi1(uy)}t = ${rienSi1(vy)}s ${ecritureAlgebrique(yB - yA)} \\\\
+            ${rienSi1(uz)}t = ${rienSi1(vz)}s ${ecritureAlgebrique(zB - zA)}
+            \\end{cases}$`
+          texteCorr += `<br><br>$\\begin{cases}
+            t = ${new FractionEtendue(vx, ux).texFractionSimplifiee}s ${new FractionEtendue(xB - xA, ux).texFractionSignee} \\\\\\\\
+            t= ${new FractionEtendue(vy, uy).texFractionSimplifiee}s ${new FractionEtendue(yB - yA, uy).texFractionSignee} \\\\\\\\
+            t = ${new FractionEtendue(vz, uz).texFractionSimplifiee}s ${new FractionEtendue(zB - zA, uz).texFractionSignee}
+            \\end{cases}$`
+          texteCorr += '<br>En égalisant les lignes 1 et 2, et les lignes 1 et 3, cela implique que $s$ doit vérifier le système :<br>'
+          texteCorr += `<br>$\\begin{cases}
+            ${new FractionEtendue(vx, ux).texFractionSimplifiee}s ${new FractionEtendue(xB - xA, ux).texFractionSignee}=${new FractionEtendue(vy, uy).texFractionSimplifiee}s ${new FractionEtendue(yB - yA, uy).texFractionSignee} \\\\\\\\
+            ${new FractionEtendue(vx, ux).texFractionSimplifiee}s ${new FractionEtendue(xB - xA, ux).texFractionSignee} = ${new FractionEtendue(vz, uz).texFractionSimplifiee}s ${new FractionEtendue(zB - zA, uz).texFractionSignee}
+            \\end{cases}$`
+          texteCorr += `<br><br>$\\begin{cases}
+            \\left(${u1.texFractionSimplifiee}-${u2.texFractionSimplifiee}\\right)s =${new FractionEtendue(yB - yA, uy).texFractionSignee}-${new FractionEtendue(xB - xA, ux).texFractionSignee} \\\\\\\\
+            \\left(${u1.texFractionSimplifiee}-${u3.texFractionSimplifiee}\\right)s = ${new FractionEtendue(zB - zA, uz).texFractionSignee}-${new FractionEtendue(xB - xA, ux).texFractionSignee}
+            \\end{cases}$`
+
+          texteCorr += `<br><br>$\\begin{cases}
+            ${u1.differenceFraction(u2)}s =${new FractionEtendue(yB - yA, uy).texFractionSignee} \\\\\\\\
+            ${u1.differenceFraction(u3)}s = ${new FractionEtendue(zB - zA, uz).texFractionSignee}
+            \\end{cases}$`
+
           break
         default: // On termine toujours par default qui ici correspond à 'type3':
           texte = `Question ${i + 1} de type 3`
