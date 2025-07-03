ba6dc6a35d35ede8646798c4a3c5ba8a15224481
gillesMora <gilles.mora@ac-normandie.fr>
2025-07-03 15:27:17 +0200

    CreationCAN

---
+++ src/exercices/can/6e/can6C50.ts
@@ -0,0 +1,55 @@
+import { choice } from '../../../lib/outils/arrayOutils'
+import { texNombre } from '../../../lib/outils/texNombre'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import Exercice from '../../Exercice'
+export const titre = 'Établir une relation entre unité, dixième, centième et millième '
+export const interactifReady = true
+export const interactifType = 'mathLive'
+export const amcReady = true
+export const amcType = 'AMCNum'
+export const dateDePublication = '03/07/2025'
+/**
+ * Modèle d'exercice très simple pour la course aux nombres
+ * @author Gilles Mora
+*/
+export const uuid = '59129'
+
+export const refs = {
+  'fr-fr': ['can6C50'],
+  'fr-ch': []
+}
+export default class RelationUniteDixiemeCentiemeMillieme extends Exercice {
+  constructor () {
+    super()
+    this.typeExercice = 'simple'
+    this.nbQuestions = 1
+    this.formatChampTexte = KeyboardType.clavierDeBase
+    this.optionsChampTexte = { texteAvant: '<br>' }
+  }
+
+  nouvelleVersion () {
+    let a, b, resultat
+    switch (choice([1, 2])) { //, 2, 3
+      case 1:
+        a = choice([1, 10, 100, 1000])
+        b = choice([10, 100, 1000])
+        resultat = texNombre(a / b, 3)
+        this.question = `Écrire sous la forme d'un décimal ou d'un entier : $\\dfrac{${texNombre(a, 0)}}{${texNombre(b, 0)}}$`
+        this.correction = `$\\dfrac{${texNombre(a, 0)}}{${texNombre(b, 0)}} = ${miseEnEvidence(resultat)}$`
+        this.reponse = resultat
+        break
+
+      case 2://
+        a = choice([10, 100, 1000])
+        b = choice([10, 100, 1000])
+        resultat = texNombre(a / b, 3)
+        this.question = `Écrire sous la forme d'un décimal ou d'un entier : $${texNombre(a, 0)}\\times\\dfrac{1}{${texNombre(b, 0)}}$`
+        this.correction = `$${texNombre(a, 0)}\\times\\dfrac{1}{${texNombre(b, 0)}} = ${miseEnEvidence(resultat)}$`
+        this.reponse = resultat
+        break
+    }
+    this.canEnonce = this.question
+    this.canReponseACompleter = ''
+  }
+}
