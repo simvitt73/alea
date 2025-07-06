075aff9553e47824acbf3dd309b814193dfff65f
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-07-06 17:44:10 +0400

    Création de la classe ExerciceSimple qui hérite pour l'instant de la classe exercice

---
+++ src/exercices/ExerciceSimple.ts
@@ -0,0 +1,11 @@
+import Exercice from './Exercice'
+
+export default class ExerciceSimple extends Exercice {
+  distracteurs: (string | number)[]
+  versionQcmDisponible?: boolean // Pour les exercices de type simple, si des distracteurs sont définis, on peut proposer une version QCM
+  versionQcm?: boolean // Seulement pour les exercices de type simple, version QCM activée si 'true'
+  constructor () {
+    super()
+    this.distracteurs = []
+  }
+}
