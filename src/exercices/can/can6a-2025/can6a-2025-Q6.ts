ed72b35c6f3611f441ee5b7497de193a328f7593
EricElter <eric.elter@ac-normandie.fr>
2025-07-18 08:49:03 +0200

    fix : can6a-2025-Q6 (correction interactif)

--- src/exercices/can/can6a-2025/can6a-2025-Q6.ts
+++ src/exercices/can/can6a-2025/can6a-2025-Q6.ts
@@ -2,7 +2,6 @@ import ExerciceSimple from '../../ExerciceSimple'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { randint } from '../../../modules/outils'
-import { handleAnswers } from '../../../lib/interactif/gestionInteractif'
 import Hms from '../../../modules/Hms'
 import { formatMinute } from '../../../lib/outils/texNombre'
 export const titre = 'Ajouter une demi-heure'
@@ -33,14 +32,13 @@ export default class Can2025N62Q6 extends ExerciceSimple {
   nouvelleVersion () {
     const h = this.canOfficielle ? 4 : randint(1, 9)
     const min = this.canOfficielle ? 55 : randint(3, 5) * 10 + 5
-    this.reponse = new Hms({ hour: h + 1, minute: min - 30 })
+    this.reponse = new Hms({ hour: h + 1, minute: min - 30 }).toString()
     this.question = `Il est $${h}$ h $${min}$ min.<br>
     Dans une demi-heure, quelle heure sera-t-il ?`
 
     this.correction = `Une demi-heure est égale à $30$ minutes. <br>
     Ainsi $${h}$ h $${min}$ min + $30$ min est égal à $${miseEnEvidence(h + 1)}$ h $${miseEnEvidence(formatMinute(min - 30))}$ min.`
 
-    handleAnswers(this, 0, { reponse: { value: new Hms({ hour: h + 1, minute: min - 30 }).toString(), options: { HMS: true } } })
     this.canEnonce = this.question
     this.canReponseACompleter = '$\\ldots$ h $\\ldots$ min'
   }
