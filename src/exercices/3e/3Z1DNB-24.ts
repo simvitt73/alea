d4809ddf776de7bc10e38662f9a1bac3cab02fe8
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2025-08-29 14:19:40 +0000

    Jc qcm2025

--- src/exercices/3e/3Z1DNB-24.ts
+++ src/exercices/3e/3Z1DNB-24.ts
@@ -1,20 +1,22 @@
-import Exercice from '../../Exercice'
-import type ExerciceQcmA from '../../ExerciceQcmA'
-import Question1 from './MJ25E3Q1'
-import Question2 from './MJ25E3Q2'
-import Question3 from './MJ25E3Q3'
-import Question4 from './MJ25E3Q4'
-import Question5 from './MJ25E3Q5'
-import Question6 from './MJ25E3Q6'
+import Exercice from '../Exercice'
+import type ExerciceQcmA from '../ExerciceQcmA'
+import Question1 from '../QCMBrevet/A2025/MJ25E3Q1'
+import Question2 from '../QCMBrevet/A2025/MJ25E3Q2'
+import Question3 from '../QCMBrevet/A2025/MJ25E3Q3'
+import Question4 from '../QCMBrevet/A2025/MJ25E3Q4'
+import Question5 from '../QCMBrevet/A2025/MJ25E3Q5'
+import Question6 from '../QCMBrevet/A2025/MJ25E3Q6'
 export const uuid = '47a56'
 export const refs = {
-  'fr-fr': [''],
+  'fr-fr': ['3Z1DNB-24'],
   'fr-ch': [],
 }
 export const interactifReady = false
 export const titre = 'Métropole Juin 2025 - Exercice 3'
-export const dateDePublication = '27/05/2025'
-
+export const dateDePublication = '27/08/2025'
+/**
+ * @author Jean-Claude Lhote
+ */
 export default class MetropoleJ25Exo3 extends Exercice {
   questions: ExerciceQcmA[] = []
   constructor() {
