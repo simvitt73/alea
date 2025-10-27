f17f80301a84ded368ab1a4404c7d099bf2b2d49
melx1998 <36272739+melx1998@users.noreply.github.com>
2025-10-27 20:12:44 +0100

    Renommer fichier

--- src/exercices/TT/TTE-2.ts
+++ src/exercices/TT/TTE-2.ts
@@ -3,14 +3,14 @@ import { arrondi } from '../../lib/outils/nombres'
 import { randint } from '../../modules/outils'
 import ExerciceSimple from '../ExerciceSimple'
 
-export const titre = 'Sens de variation de $x \\mapsto ka^{x} $' // Ce titre est nécessaire et doit commencer par un verbe à l'infinitif
+export const titre = "Taux moyen d'évolution" // Ce titre est nécessaire et doit commencer par un verbe à l'infinitif
 
 // Les éléménts ci-dessous sont nécessaires pour la prise en compte d'un champ classique pour l'interactivité
 export const interactifReady = true
 export const interactifType = 'mathLive'
 
 // Toujours utile cette information
-export const dateDePublication = '03/11/2025'
+export const dateDePublication = '18/10/2025'
 
 /* Les éléments ci-dessous sont indispensables pour le référencement d'un exercice
          - uuid : C'est la plaque d'identité de l'exercice. Cette référence, une fois fournie, ne sera plus jamais changée et permettra
@@ -18,10 +18,10 @@ export const dateDePublication = '03/11/2025'
           - refs : c'est l'identité de l'exercice pour être plus facilement identifiable par l'utilisateur.
                   Il y a une référence française et suisse
 */
-export const uuid = '0b7dd' // Cet élement doit être différent d un exercice à l autre. Lancer pnpm start pour copier l uuid
+export const uuid = 'ef3d7' // Cet élement doit être différent d un exercice à l autre. Lancer pnpm start pour copier l uuid
 // aléatoire proposé par le moteur
 export const refs = {
-  'fr-fr': ['Exemple'], // Cet élément doit être choisi en lien avec le référentiel
+  'fr-fr': ['TTE-2'], // Cet élément doit être choisi en lien avec le référentiel
   'fr-ch': [], // Cet élément peut rester vide et sera géré ultérieurement par Nathan, qui gère le référentiel suisse.
 }
 
