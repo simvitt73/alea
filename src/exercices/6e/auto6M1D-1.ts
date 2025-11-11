da0cc9e2cae5c73acb83ca4543133f9cc6caaf80
Guillaume Valmont <guillaume.valmont@ac-reunion.fr>
2025-11-11 21:19:38 +0400

    Ajout de l'exercice auto6M1D-1

---
+++ src/exercices/6e/auto6M1D-1.ts
@@ -0,0 +1,157 @@
+import { droiteGraduee } from '../../lib/2d/DroiteGraduee'
+import { fixeBordures } from '../../lib/2d/fixeBordures'
+import { combinaisonListes } from '../../lib/outils/arrayOutils'
+import { range } from '../../lib/outils/nombres'
+import FractionEtendue from '../../modules/FractionEtendue'
+import { mathalea2d } from '../../modules/mathalea2d'
+import {
+  gestionnaireFormulaireTexte,
+  listeQuestionsToContenu,
+  randint,
+} from '../../modules/outils'
+import Exercice from '../Exercice'
+
+export const titre =
+  "Placer une fraction sur une droite graduée à partir d'une fraction unitaire"
+
+export const dateDePublication = '11/11/2025'
+
+/**
+ * @author Guillaume Valmont
+ */
+export const uuid = 'ab11c'
+
+export const refs = {
+  'fr-fr': ['auto6M1D-1'],
+  'fr-2016': [],
+  'fr-ch': [],
+}
+export default class sensDesPrefixes extends Exercice {
+  constructor() {
+    super()
+
+    this.nbQuestions = 4
+
+    this.sup = 0
+    this.besoinFormulaireNumerique = [
+      'Type de questions',
+      3,
+      '1 : Mélange\n2 : Fractions < 1\n3 : Fractions > 1',
+    ]
+  }
+
+  nouvelleVersion() {
+    const typesDeQuestionsDisponibles = gestionnaireFormulaireTexte({
+      saisie: this.sup,
+      min: 2,
+      max: 3,
+      melange: 1,
+      defaut: 1,
+      nbQuestions: this.nbQuestions,
+    })
+    const listeTypeDeQuestions = combinaisonListes(
+      typesDeQuestionsDisponibles,
+      this.nbQuestions,
+    )
+    const fractionInf1Uniquement = listeTypeDeQuestions.every((t) => t === 2)
+
+    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50; ) {
+      const maxUnit = 3
+      const fractionSup1 = listeTypeDeQuestions[i] === 3
+      const den = randint(3, 10)
+      const num = fractionSup1
+        ? randint(den + 1, den * maxUnit - 1, [den * 2])
+        : randint(1, den - 1)
+      const fraction = new FractionEtendue(num, den)
+      const fractionUnitaire = new FractionEtendue(1, den)
+      const paramsDroite: ParamsDroite = {
+        Min: 0,
+        Max: fractionInf1Uniquement ? 1 : maxUnit,
+        Unite: 5,
+        axeEpaisseur: 0.5,
+        labelsPrincipaux: true,
+        labelListe: [
+          [fractionUnitaire.toNumber(), fractionUnitaire.texFraction],
+        ],
+        labelCustomDistance: 1,
+        labelDistance: 1,
+        pointListe: [[fractionUnitaire.toNumber(), '']],
+        pointCouleur: 'black',
+        pointStyle: '|',
+        labelListeScriptsize: true,
+      }
+      const droiteEnonce = droiteGraduee(paramsDroite)
+      const droiteCorrection = droiteGraduee(
+        Object.assign({}, paramsDroite, {
+          pointListe: [
+            ...range(num - 1).map((k) => [
+              fractionUnitaire.toNumber() * (k + 1),
+              '',
+            ]),
+          ],
+          labelListe: [
+            [fractionUnitaire.toNumber(), fractionUnitaire.texFraction],
+            [fraction.toNumber(), fraction.texFraction],
+          ],
+        }),
+      )
+      const droiteCorrectionOpti = droiteGraduee(
+        Object.assign({}, paramsDroite, {
+          pointListe: [
+            ...range((num % den) - 1).map((k) => [
+              fractionUnitaire.toNumber() * (k + 1) + Math.floor(num / den),
+              '',
+            ]),
+            [fractionUnitaire.toNumber(), ''],
+          ],
+          labelListe: [
+            [fractionUnitaire.toNumber(), fractionUnitaire.texFraction],
+            [fraction.toNumber(), fraction.texFraction],
+          ],
+        }),
+      )
+      let texte = `Placer la fraction $${fraction.texFraction}$ en s'aidant de la fraction unitaire $${fractionUnitaire.texFraction}$ déjà placée sur la droite graduée ci-dessous.<br>`
+      texte += mathalea2d(
+        Object.assign({}, fixeBordures([droiteEnonce])),
+        droiteEnonce,
+      )
+      let texteCorr = `$${fraction.texFraction} = ${num} \\times ${fractionUnitaire.texFraction}$ donc
+pour placer $${fraction.texFraction}$ il faut faire $${num}$ fois la distance entre $0$ et $${fractionUnitaire.texFraction}$.<br>
+On peut utiliser le compas pour reporter cette distance $${num}$ fois sur la droite graduée.<br>`
+      texteCorr += mathalea2d(
+        Object.assign({}, fixeBordures([droiteCorrection])),
+        droiteCorrection,
+      )
+      if (fractionSup1) {
+        texteCorr += `<br><br>On peut aussi remarquer que $${fraction.texFraction} = ${Math.floor(fraction.toNumber())} + \\dfrac{${num % den}}{${den}}$.<br>
+Il suffit donc de reporter $${num % den}$ fois la distance entre $0$ et $${fractionUnitaire.texFraction}$ à partir de $${Math.floor(fraction.toNumber())}$.<br>`
+        texteCorr += mathalea2d(
+          Object.assign({}, fixeBordures([droiteCorrectionOpti])),
+          droiteCorrectionOpti,
+        )
+      }
+      if (this.questionJamaisPosee(i, texte)) {
+        this.listeQuestions[i] = texte
+        this.listeCorrections[i] = texteCorr
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenu(this)
+  }
+}
+
+type ParamsDroite = {
+  Min: number
+  Max: number
+  Unite: number
+  axeEpaisseur: number
+  labelsPrincipaux: boolean
+  labelListe: [number, string][]
+  labelCustomDistance: number
+  labelDistance: number
+  pointListe: [number, string][]
+  pointCouleur: string
+  pointStyle: string
+  labelListeScriptsize: boolean
+}
