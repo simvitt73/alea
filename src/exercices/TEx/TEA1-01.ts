e75aaad218acdd557abd4e1c457f301f648d092d
Stéphane Guyon <stephane.guyon@ac-montpelier.fr>
2025-11-26 11:51:44 +0100

    Update TEA1-01.ts

--- src/exercices/TEx/TEA1-01.ts
+++ src/exercices/TEx/TEA1-01.ts
@@ -31,32 +31,29 @@ export default class ExerciceCongruence extends Exercice {
       // a et k aléatoires
       const k = randint(3, 10)
 
-      const a = randint(20, 50)
+     
+      let a: number
+do a = randint(-50, 50);
+while (a >= -9 && a <= 9);
 
       // plus petit entier positif ou nul b tel que a ≡ b [k]
       let r = a % k
       if (r < 0) r += k // reste dans [0, k-1]
       const b = r === 0 ? k : r
       const q = (a - r) / k
-      texte =
-        `Déterminer le plus petit entier positif ou nul $a$ vérifiant cette égalité $${a} \\equiv a~[${k}]$.`
-
-      texteCorr += `On effectue la division euclidienne de $${a}$ par $${k}$ :<br>`
-      texteCorr += `$${a} = ${k}\\times${q}$`
+      texte =`Déterminer le plus petit entier positif ou nul $a$ vérifiant cette égalité $${a} \\equiv a \\,\\, [${k}]$.`
+     
       if (r !== 0) {
-        texteCorr += `$+ ${r}$ avec $0 \\leqslant ${r} < ${k}$.<br>`
-      }
-      texteCorr += `<br>Le reste est donc égal à $${r}$. <br>`
-
-if (r !== 0) {
-  texteCorr += `On a donc $${a} \\equiv${miseEnEvidence(`${r}`)}[${k}]$.`
-}
+         texteCorr += `On effectue la division euclidienne de $${a}$ par $${k}$ :<br>`
+      texteCorr += `$${a} = ${k}\\times${q} + ${r}$ . <br> 
+        L'inégalité $0 \\leqslant ${r} < ${k}$ garantit que $${r}$ est bien le reste de la division euclidienne.<br>
+        On a donc $${a} \\equiv ${r} \\,\\,[${k}]$.`}
+    
        else {
         texteCorr +=
-          `Le plus petit entier positif ou nul congru à $${a}$ modulo $${k}$ est ` +
-          `$${miseEnEvidence(`b`)}$.`
+          `On observe que $${b}\\mid ${a}$. <br>On a donc $${a} \\equiv  0\\,\\,[${k}]$.`
       }
-
+      texteCorr +=`<br>Le plus petit entier positif ou nul $a$ vérifiant cette égalité est donc $${miseEnEvidence(`${r}`)}$.`
       if (this.questionJamaisPosee(i, texte)) {
         this.listeQuestions[i] =
           texte +
