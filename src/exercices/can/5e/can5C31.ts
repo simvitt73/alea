dc6487868edccead09a336ede917ee22b1fb6743
Eric Elter <eric.elter@ac-normandie.fr>
2026-01-01 14:50:46 +0000

    can5C31 : Amélioration interactivité

--- src/exercices/can/5e/can5C31.ts
+++ src/exercices/can/5e/can5C31.ts
@@ -12,13 +12,12 @@ export const dateDePublication = '30/12/2025'
 
 /**
  * @author Gilles Mora
-
  */
 
 export const uuid = 'c70e1'
 
 export const refs = {
-  'fr-fr': ['can5C31'],
+  'fr-fr': ['can5C31, 3autoP07-2'],
   'fr-ch': [],
 }
 export default class ProblemeFraction extends ExerciceSimple {
@@ -26,52 +25,61 @@ export default class ProblemeFraction extends ExerciceSimple {
     super()
     this.typeExercice = 'simple'
     this.nbQuestions = 1
-    this.spacing=1.5
-    this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
-     this.optionsDeComparaison = {
-      fractionEgale: true,
+    this.spacing = 1.5
+    this.formatChampTexte = KeyboardType.clavierNumbers
+    this.optionsDeComparaison = {
+      nombreDecimalSeulement: true,
     }
- this.optionsChampTexte = {
-              texteAvant: '<br>',
-              texteApres: `  g.`,
-            }
+    this.optionsChampTexte = {
+      texteAvant: '<br>',
+      texteApres: `  g.`,
+    }
   }
 
   nouvelleVersion() {
-     switch (choice([1, 2])) {
+    switch (choice([1, 2])) {
       case 1:
         {
- const listeFractions: [string, number, number][] = [
-  ['un tiers', 1, 3], ['un quart', 1, 4], ['un cinquième', 1, 5], ['un sixième', 1, 6]
-]
-const fractionChoisie = choice(listeFractions)
-    this.reponse = fractionChoisie[2]
-    this.question = `Dans ${fractionChoisie[0]} de sandwich, il y a un gramme de sel.<br>
+          const listeFractions: [string, number, number][] = [
+            ['un tiers', 1, 3],
+            ['un quart', 1, 4],
+            ['un cinquième', 1, 5],
+            ['un sixième', 1, 6],
+          ]
+          const fractionChoisie = choice(listeFractions)
+          this.reponse = fractionChoisie[2]
+          this.question = `Dans ${fractionChoisie[0]} de sandwich, il y a un gramme de sel.<br>
     Combien y a-t-il de grammes de sel dans un sandwich entier ?`
-   
-    this.correction = `Dans ${fractionChoisie[0]} de sandwich, il y a un gramme de sel, donc dans un sandwich entier, il y en a $${fractionChoisie[2]}$ fois plus, donc $${miseEnEvidence(this.reponse)}$ g.`
- }
-    break
+
+          this.correction = `Dans ${fractionChoisie[0]} de sandwich, il y a un gramme de sel, donc dans un sandwich entier, il y en a $${fractionChoisie[2]}$ fois plus, donc $${miseEnEvidence(this.reponse)}$ g.`
+        }
+        break
 
       case 2:
-        default:
-            {
- const listeFractions2: [string, number, number, string][] = [
-  ['deux tiers', 2, 3, 'tiers'], ['trois quarts', 3, 4, 'quart'], ['quatre cinquièmes', 4, 5, 'cinquième'], ['cinq sixièmes', 5, 6, 'sixième']
-]
-const fractionChoisie2 = choice(listeFractions2)
-    this.reponse =  new FractionEtendue(fractionChoisie2[2]*30,fractionChoisie2[1])
-    this.question = `Dans ${fractionChoisie2[0]} de gâteau, il y a trente grammes de sucre.<br>
+      default:
+        {
+          const listeFractions2: [string, number, number, string][] = [
+            ['deux tiers', 2, 3, 'tiers'],
+            ['trois quarts', 3, 4, 'quart'],
+            ['quatre cinquièmes', 4, 5, 'cinquième'],
+            ['cinq sixièmes', 5, 6, 'sixième'],
+          ]
+          const fractionChoisie2 = choice(listeFractions2)
+          this.reponse = new FractionEtendue(
+            fractionChoisie2[2] * 30,
+            fractionChoisie2[1],
+          )
+          this.question = `Dans ${fractionChoisie2[0]} de gâteau, il y a trente grammes de sucre.<br>
     Combien y a-t-il de grammes de sucre dans un gâteau entier ?`
- 
-   this.correction = `Dans ${fractionChoisie2[0]} de gâteau, il y a $30$ g de sucre, donc dans un ${fractionChoisie2[3]} de gâteau, 
-   il y a $30 \\div ${fractionChoisie2[1]} = ${texNombre(new FractionEtendue(30 , fractionChoisie2[1]).valeurDecimale)}$ g de sucre.<br>
+
+          this.correction = `Dans ${fractionChoisie2[0]} de gâteau, il y a $30$ g de sucre, donc dans un ${fractionChoisie2[3]} de gâteau, 
+   il y a $30 \\div ${fractionChoisie2[1]} = ${texNombre(new FractionEtendue(30, fractionChoisie2[1]).valeurDecimale)}$ g de sucre.<br>
    Donc, dans un gâteau entier, 
-   il y a $${texNombre(new FractionEtendue(30 , fractionChoisie2[1]).valeurDecimale)} \\times ${fractionChoisie2[2]} = ${miseEnEvidence(texNombre(this.reponse.valeurDecimale))}$ g.`
-} 
-  break
-     }
-  this.canEnonce = this.question
-    this.canReponseACompleter = '$\\ldots$ g' 
+   il y a $${texNombre(new FractionEtendue(30, fractionChoisie2[1]).valeurDecimale)} \\times ${fractionChoisie2[2]} = ${miseEnEvidence(texNombre(this.reponse.valeurDecimale))}$ g.`
+        }
+        break
+    }
+    this.canEnonce = this.question
+    this.canReponseACompleter = '$\\ldots$ g'
   }
 }
