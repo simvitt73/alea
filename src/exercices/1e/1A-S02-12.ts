7774ad2ab500dd42cee2b1e4a8e418aca7bc4518
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2026-01-01 09:30:04 +0000

    Jc fait des stats

---
+++ src/exercices/1e/1A-S02-12.ts
@@ -0,0 +1,119 @@
+import Stat from '../../lib/mathFonctions/Stat'
+import { choice } from '../../lib/outils/arrayOutils'
+import { texNombre } from '../../lib/outils/texNombre'
+import { creerSerieDeMoyenneEtEtendue } from '../../modules/outilsStat'
+import { nombreElementsDifferents } from '../ExerciceQcm'
+import ExerciceQcmA from '../ExerciceQcmA'
+
+export const uuid = '3ba2a'
+export const refs = {
+  'fr-fr': ['1A-S02-12'],
+  'fr-ch': [],
+}
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+export const titre = "Calculer un pourcentage à partir d'un diagramme"
+export const dateDePublication = '31/12/2025'
+/**
+ * @author Jean-claude Lhote
+ */
+
+export default class CalculPourcentageQCM extends ExerciceQcmA {
+  // Ceci est la fonction qui s'occupe d'écrire l'énoncé, la correction et les réponses
+  // Elle factorise le code qui serait dupliqué dans versionAleatoire et versionOriginale
+  private appliquerLesValeurs(
+    serie: [number, number][] | number[],
+    noteMax: number,
+    valeurCible: number,
+  ): void {
+    const maSerie = new Stat(serie)
+    const histogramme = maSerie.diagramme({
+      cumul: false,
+      barres: true,
+      valuesOn: true,
+    })
+    const effectifCible =
+      maSerie.serieTableau.find(([note]) => note === valeurCible)?.[1] ?? 0
+    const n = maSerie.serie.length
+
+    const pourCent = Math.round((effectifCible / n) * 100)
+    const distracteur1 = choice(
+      [pourCent - 10, pourCent + 10, pourCent - 15, pourCent + 15].filter(
+        (v) => v > 0 && v < 100,
+      ),
+      pourCent,
+    )
+    const distracteur2 = choice(
+      [pourCent - 5, pourCent + 5, pourCent - 20, pourCent + 20].filter(
+        (v) => v > 0 && v < 100,
+      ),
+      [pourCent, distracteur1],
+    )
+    const distracteur3 = choice(
+      [
+        pourCent - 10,
+        pourCent + 10,
+        pourCent - 15,
+        pourCent + 15,
+        pourCent - 5,
+        pourCent + 5,
+        pourCent - 20,
+        pourCent + 20,
+      ].filter((v) => v > 0 && v < 100),
+      [pourCent, distracteur1, distracteur2],
+    )
+
+    this.reponses = [
+      `${texNombre(pourCent, 0)}\\%`,
+      `${texNombre(distracteur1, 0)}\\%`,
+      `${texNombre(distracteur2, 0)}\\%`,
+      `${texNombre(distracteur3, 0)}\\%`,
+    ].map((r) => `$${r}$`)
+    this.enonce = `Voici la répartition des notes sur ${noteMax} d'une classe de première.<br>
+      ${histogramme}<br><br>
+      Quel est le pourcentage d'élèves ayant obtenu la note ${valeurCible} ?`
+
+    // Correction : explication simple, claire
+    this.correction = `Le pourcentage d'élèves ayant obtenu la note ${valeurCible} est calculé en divisant l'effectif de cette note par l'effectif total, puis en multipliant par 100.<br>
+      L'effectif total est le nombre de notes représentées dans l'histogramme.<br>
+      Ici, on trouve un effectif total de $${n}$ élèves.<br>
+      L'effectif des élèves ayant obtenu la note ${valeurCible} est de $${maSerie.serieTableau.find(([note]) => note === valeurCible)?.[1] ?? 0}$.<br>
+      Donc le pourcentage est de $\\dfrac{${maSerie.serieTableau.find(([note]) => note === valeurCible)?.[1] ?? 0}}{${n}} \\times 100 = ${pourCent}\\%$.`
+  }
+
+  versionOriginale: () => void = () => {
+    const maSerie: [number, number][] = [
+      [1, 4],
+      [2, 8],
+      [3, 7],
+      [4, 5],
+      [5, 1],
+    ]
+    this.appliquerLesValeurs(maSerie, 5, 4)
+  }
+
+  versionAleatoire: () => void = () => {
+    const n = 4 // Nombre de réponses différentes souhaitées
+    do {
+      const noteMax = choice([10, 20])
+      const maSerie = creerSerieDeMoyenneEtEtendue({
+        mean: noteMax / 2,
+        range: noteMax / 2,
+        n: choice([20, 25, 10]),
+        isInteger: true,
+        gaussian: true,
+      })
+      const valeurCible = choice(maSerie)
+      this.appliquerLesValeurs(maSerie, noteMax, valeurCible)
+    } while (nombreElementsDifferents(this.reponses) < n)
+  }
+
+  // Ici il n'y a rien à faire, on appelle juste la version aleatoire (pour un qcm aleatoirisé, c'est le fonctionnement par défaut)
+  constructor() {
+    super()
+    this.options = { vertical: true, ordered: false }
+    this.versionAleatoire()
+  }
+}
---
+++ src/exercices/1e/1A-S02-13.ts
@@ -0,0 +1,119 @@
+import Stat from '../../lib/mathFonctions/Stat'
+import { choice } from '../../lib/outils/arrayOutils'
+import { texNombre } from '../../lib/outils/texNombre'
+import { creerSerieDeMoyenneEtEtendue } from '../../modules/outilsStat'
+import { nombreElementsDifferents } from '../ExerciceQcm'
+import ExerciceQcmA from '../ExerciceQcmA'
+
+export const uuid = '4ba2a'
+export const refs = {
+  'fr-fr': ['1A-S02-13'],
+  'fr-ch': [],
+}
+export const interactifReady = true
+export const interactifType = 'qcm'
+export const amcReady = 'true'
+export const amcType = 'qcmMono'
+export const titre = "Calculer le pourcentage d'élèves obtenant la moyenne"
+export const dateDePublication = '31/12/2025'
+/**
+ * @author Jean-claude Lhote
+ */
+
+export default class CalculPourcentageEtMoyenneQCM extends ExerciceQcmA {
+  // Ceci est la fonction qui s'occupe d'écrire l'énoncé, la correction et les réponses
+  // Elle factorise le code qui serait dupliqué dans versionAleatoire et versionOriginale
+  private appliquerLesValeurs(
+    serie: [number, number][] | number[],
+    noteMax: number,
+  ): void {
+    const valeurCible = noteMax / 2
+    const maSerie = new Stat(serie)
+    const histogramme = maSerie.diagramme({
+      cumul: false,
+      barres: true,
+      valuesOn: true,
+    })
+    const effectifCible = maSerie.serieTableau
+      .filter(([note]) => Number(note) >= valeurCible)
+      .reduce((acc, [, effectif]) => acc + effectif, 0)
+    const n = maSerie.serie.length
+
+    const pourCent = Math.round((effectifCible / n) * 100)
+    const distracteur1 = choice(
+      [pourCent - 10, pourCent + 10, pourCent - 15, pourCent + 15].filter(
+        (v) => v > 0 && v < 100,
+      ),
+      pourCent,
+    )
+    const distracteur2 = choice(
+      [pourCent - 5, pourCent + 5, pourCent - 20, pourCent + 20].filter(
+        (v) => v > 0 && v < 100,
+      ),
+      [pourCent, distracteur1],
+    )
+    const distracteur3 = choice(
+      [
+        pourCent - 10,
+        pourCent + 10,
+        pourCent - 15,
+        pourCent + 15,
+        pourCent - 5,
+        pourCent + 5,
+        pourCent - 20,
+        pourCent + 20,
+      ].filter((v) => v > 0 && v < 100),
+      [pourCent, distracteur1, distracteur2],
+    )
+
+    this.reponses = [
+      `${texNombre(pourCent, 0)}\\%`,
+      `${texNombre(distracteur1, 0)}\\%`,
+      `${texNombre(distracteur2, 0)}\\%`,
+      `${texNombre(distracteur3, 0)}\\%`,
+    ].map((r) => `$${r}$`)
+    this.enonce = `Voici la répartition des notes sur ${noteMax} d'une classe de première.<br>
+      ${histogramme}<br><br>
+      Quel est le pourcentage d'élèves ayant obtenu la moyenne ?`
+
+    // Correction : explication simple, claire
+    this.correction = `Le pourcentage d'élèves ayant obtenu la moyenne est calculé en divisant l'effectif des élèves ayant obtenu une note supérieure ou égale à la moyenne par l'effectif total, puis en multipliant par 100.<br>
+      L'effectif total est le nombre de notes représentées dans l'histogramme.<br>
+      Ici, on trouve un effectif total de $${n}$ élèves.<br>
+      L'effectif des élèves ayant obtenu une note supérieure ou égale à la moyenne est de $${effectifCible}$.<br>
+      Donc le pourcentage est de $\\dfrac{${effectifCible}}{${n}} \\times 100 = ${pourCent}\\%$.`
+  }
+
+  versionOriginale: () => void = () => {
+    const maSerie: [number, number][] = [
+      [1, 4],
+      [2, 8],
+      [3, 7],
+      [4, 5],
+      [5, 1],
+    ]
+    this.appliquerLesValeurs(maSerie, 5)
+  }
+
+  versionAleatoire: () => void = () => {
+    const n = 4 // Nombre de réponses différentes souhaitées
+    do {
+      const noteMax = choice([10, 20])
+      const maSerie = creerSerieDeMoyenneEtEtendue({
+        mean: noteMax / 2,
+        range: noteMax / 2,
+        n: choice([20, 25, 10]),
+        isInteger: true,
+        gaussian: true,
+      })
+      this.appliquerLesValeurs(maSerie, noteMax)
+    } while (nombreElementsDifferents(this.reponses) < n)
+  }
+
+  // Ici il n'y a rien à faire, on appelle juste la version aleatoire (pour un qcm aleatoirisé, c'est le fonctionnement par défaut)
+  constructor() {
+    super()
+    this.options = { vertical: true, ordered: false }
+    this.versionAleatoire()
+  }
+}
