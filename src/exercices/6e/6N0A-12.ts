2fa8cd39408ac090f75cff246eccca459724d0a3
Jean-Claude Lhote <jean-claude.lhote@ac-nancy-metz.fr>
2026-01-12 05:55:39 +0000

    Resolve "Création exercice Multiplier en utilisant la distributivité"

---
+++ src/exercices/6e/6N0A-12.ts
@@ -0,0 +1,225 @@
+import { tableauColonneLigne } from '../../lib/2d/tableau'
+import { KeyboardType } from '../../lib/interactif/claviers/keyboard'
+import { handleAnswers } from '../../lib/interactif/gestionInteractif'
+import { toutPourUnPoint } from '../../lib/interactif/mathLive'
+import { ajouteChampTexteMathLive } from '../../lib/interactif/questionMathLive'
+import { AddTabDbleEntryMathlive } from '../../lib/interactif/tableaux/AjouteTableauMathlive'
+import { choice, shuffle } from '../../lib/outils/arrayOutils'
+import { miseEnEvidence } from '../../lib/outils/embellissements'
+import { rangeMinMax } from '../../lib/outils/nombres'
+import { lettreDepuisChiffre } from '../../lib/outils/outilString'
+import { texNombre } from '../../lib/outils/texNombre'
+import { context } from '../../modules/context'
+import {
+  gestionnaireFormulaireTexte,
+  listeQuestionsToContenuSansNumero,
+  randint,
+} from '../../modules/outils'
+import Exercice from '../Exercice'
+export const titre = 'Multiplier deux entiers grâce à la distributivité'
+export const dateDePublication = '10/01/2026'
+// export const dateDeModifImportante = '04/01/2025'
+export const interactifReady = true
+export const interactifType = 'mathLive'
+/**
+ * Développer des expressions de simple distributivité à l'aide d'un tableau de  multiplication
+ * @author Jean-Claude Lhote
+ */
+
+export const uuid = 'de189'
+
+export const refs = {
+  'fr-fr': ['6N0A-12'],
+  'fr-ch': [''],
+}
+export default class TableSimpleDistributivite extends Exercice {
+  constructor() {
+    super()
+
+    this.spacing = context.isHtml ? 1 : 2
+    this.spacingCorr = context.isHtml ? 1 : 2
+    this.nbQuestions = 4
+
+    this.exoCustomResultat = true
+    this.besoinFormulaireTexte = [
+      'Nombre de chiffres du second facteur',
+      'Nombres de 2 à 4 séparés par des tirets',
+    ]
+    this.sup = '3'
+  }
+
+  nouvelleVersion() {
+    this.answers = {}
+    this.consigne =
+      this.nbQuestions > 1
+        ? 'Dans chaque cas, compléter les tables de multiplication puis écrire le résultat de la multiplication.'
+        : 'Compléter la table de multiplication puis écrire le résultat de la multiplication.'
+    const nbChiffresSecondFacteur = gestionnaireFormulaireTexte({
+      saisie: this.sup,
+      nbQuestions: this.nbQuestions,
+      min: 2,
+      max: 4,
+      melange: 0,
+      defaut: 3,
+    }).map(Number)
+
+    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50; ) {
+      const nbChiffres = nbChiffresSecondFacteur[i]
+
+      const chiffres = shuffle(rangeMinMax(1, 9)).slice(0, nbChiffres)
+      const a = randint(2, 9)
+      const produits = [
+        chiffres[0] || 0,
+        chiffres[1] || 0,
+        chiffres[2] || 0,
+        chiffres[3] || 0,
+      ]
+        .map((x) => x * Math.pow(10, chiffres.indexOf(x)))
+        .reverse()
+        .filter((x) => x !== 0)
+      const b = produits.reduce((acc, val) => acc + val, 0)
+      let texte = ''
+      let texteCorr = ''
+
+      let tableau: TableauData = {
+        entetesCol: [],
+        entetesLgn: [],
+        contenu: [],
+        L1C1txt: '',
+        L1C2txt: '',
+        L1C3txt: '',
+        L1C4txt: '',
+      }
+
+      this.autoCorrection[2 * i] = {}
+      this.autoCorrection[2 * i + 1] = {}
+
+      texte = `$${lettreDepuisChiffre(i + 1)} = ${choice([true, false]) ? `${a}\\times ${b}` : `${b}\\times ${a}`}$`
+      texte += '<br><br>'
+      texteCorr = texte
+      // developpements = faitDeveloppement(a1, a0, b, c)
+      tableau = faitTableau(a, produits)
+      // texte += context.isHtml ? '<br>' : '\\par\\medskip'
+
+      if (this.interactif) {
+        const tableauVide = AddTabDbleEntryMathlive.convertTclToTableauMathlive(
+          tableau.entetesCol,
+          tableau.entetesLgn,
+          produits.map(() => ''),
+        )
+        // const NumEx = this.numeroExercice ? this.numeroExercice : 1
+        // OM:  fonctionne pas
+        const tabMathlive = AddTabDbleEntryMathlive.create(
+          this.numeroExercice ?? 0,
+          2 * i,
+          tableauVide,
+          `tableauMathlive`, // ` ${KeyboardType.clavierDeBaseAvecVariable}`,
+          true,
+          { L0C0: 'red' },
+        )
+        texte += tabMathlive.output
+      } else {
+        texte += tableauColonneLigne(
+          tableau.entetesCol,
+          tableau.entetesLgn,
+          tableau.contenu,
+          1.7,
+          true,
+          this.numeroExercice,
+          i,
+          false,
+          { L0C0: 'red' },
+        )
+      }
+
+      texte += '<br><br>Résultat : '
+      texte += ajouteChampTexteMathLive(
+        this,
+        2 * i + 1,
+        KeyboardType.clavierDeBase,
+      )
+
+      texteCorr += tableauColonneLigne(
+        tableau.entetesCol,
+        tableau.entetesLgn,
+        produits.map((x) => miseEnEvidence(texNombre(a * x, 0))),
+        1,
+        true,
+        this.numeroExercice,
+        2 * i,
+        false,
+        { L0C0: 'red' },
+      )
+
+      texteCorr += `<br><br>Résultat : $${lettreDepuisChiffre(i + 1)} =${produits.map((x) => texNombre(a * x, 0)).join('+')} =${miseEnEvidence(a * b)}$`
+
+      handleAnswers(this, 2 * i, {
+        bareme: toutPourUnPoint,
+        L1C1: {
+          value: tableau.L1C1txt,
+          options: {
+            calculFormel: true,
+          },
+        },
+        L1C2: {
+          value: tableau.L1C2txt,
+          options: {
+            calculFormel: true,
+          },
+        },
+        L1C3: {
+          value: tableau.L1C3txt,
+          options: {
+            calculFormel: true,
+          },
+        },
+      })
+      handleAnswers(this, 2 * i + 1, {
+        reponse: {
+          value: (a * b).toString(),
+          options: {
+            avecSigneMultiplier: false,
+          },
+        },
+      })
+
+      if (this.questionJamaisPosee(i, a, b)) {
+        // Si la question n'a jamais été posée, on en créé une autre
+        this.listeQuestions[i] = texte
+        this.listeCorrections[i] = texteCorr
+        i++
+      }
+      cpt++
+    }
+    listeQuestionsToContenuSansNumero(this)
+  }
+}
+
+interface TableauData {
+  entetesCol: string[]
+  entetesLgn: string[]
+  contenu: string[]
+  L1C1txt: string
+  L1C2txt: string
+  L1C3txt: string
+  L1C4txt?: string
+}
+
+function faitTableau(a: number, chiffres: number[]): TableauData {
+  const entetesCol: string[] = ['\\phantom{A.}\\times\\phantom{E.}']
+  const chiffresNonNuls = chiffres.filter((x) => x !== 0)
+  const nbChiffres = chiffresNonNuls.length
+  for (let i = 0; i < chiffresNonNuls.length; i++) {
+    entetesCol.push(texNombre(chiffres[i], 0))
+  }
+  const tableau: TableauData = {
+    entetesCol,
+    entetesLgn: [texNombre(a, 0)],
+    contenu: chiffresNonNuls.map((x) => '\\phantom{E.}'),
+    L1C1txt: texNombre(chiffres[0], 0),
+    L1C2txt: texNombre(chiffres[1], 0),
+    L1C3txt: nbChiffres > 2 ? texNombre(chiffres[2], 0) : '',
+    L1C4txt: nbChiffres > 3 ? texNombre(chiffres[3], 0) : '',
+  }
+  return tableau
+}
