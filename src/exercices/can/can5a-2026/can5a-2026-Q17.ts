40e1d7dea6730f2d013389ada89bb48f73ad0791
gillesMora <gilles.mora@ac-normandie.fr>
2026-01-16 16:56:18 +0100

    Q17

--- src/exercices/can/can5a-2026/can5a-2026-Q17.ts
+++ src/exercices/can/can5a-2026/can5a-2026-Q17.ts
@@ -1,5 +1,5 @@
 import { codageAngleDroit } from '../../../lib/2d/CodageAngleDroit'
-import { point } from '../../../lib/2d/PointAbstrait'
+import {  pointAbstrait } from '../../../lib/2d/PointAbstrait'
 import { polygoneAvecNom } from '../../../lib/2d/polygones'
 import { segment } from '../../../lib/2d/segmentsVecteurs'
 import { latex2d } from '../../../lib/2d/textes'
@@ -9,10 +9,9 @@ import { choice } from '../../../lib/outils/arrayOutils'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
 import { texNombre } from '../../../lib/outils/texNombre'
 import { mathalea2d } from '../../../modules/mathalea2d'
-import { randint } from '../../../modules/outils'
 import ExerciceCan from '../../ExerciceCan'
 
-export const titre = 'Calculer le périmètre d\'un rectangle'
+export const titre = 'Calculer l\'aire d\'un triangle à partir de la base et de la hauteur'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const uuid = '3hsdf'
@@ -26,15 +25,21 @@ export const refs = {
 
 */
 export default class Can52026Q17 extends ExerciceCan {
-  enonce(AB?: number, CH?: number, AH?: number, AC?: number, BC?: number) {
+ enonce(AB?: number, CH?: number, AH?: number, AC?: number, BC?: number) {
     if (AB == null || CH == null || AH == null || AC == null || BC == null) {
-      // Configurations valides avec aire entière
+      // Configurations avec aire entière
+      // [AB, CH, AH, AC, BC] avec aire = (AB × CH) / 2
       const listeCas = [
-        [10, 4, 6, 5, 8],   // Aire = 20
-        [12, 5, 4, 6.4, 9.4], // Aire = 30 (à vérifier les côtés)
-        [8, 6, 3, 6.7, 7.2],  // Aire = 24 (à vérifier les côtés)
-        [6, 4, 3, 5, 5],      // Aire = 12 (triangle isocèle)
-        [14, 4, 7, 8.1, 8.1], // Aire = 28 (triangle isocèle, à vérifier)
+        [10, 4, 3, 5, 8],      // Aire = 20 (cas officiel)
+        [6, 4, 2, 4.5, 5.7],   // Aire = 12
+        [8, 6, 4, 7, 7],   // Aire = 24 (isocèle)
+        [12, 5, 6, 7.8, 7.8],  // Aire = 30 (isocèle)
+        [14, 6, 7, 9, 9],  // Aire = 42 (isocèle)
+        [16, 4, 8, 9, 9],  // Aire = 32 (isocèle)
+        [10, 6, 5, 8, 8],  // Aire = 30 (isocèle)
+        [12, 4, 6, 7, 7],  // Aire = 24 (isocèle)
+        [8, 5, 4, 6.4, 6.4],   // Aire = 20 (isocèle)
+        [14, 4, 7, 8, 8],  // Aire = 28 (isocèle)
       ]
       const cas = choice(listeCas)
       AB = cas[0]
@@ -44,38 +49,38 @@ export default class Can52026Q17 extends ExerciceCan {
       BC = cas[4]
     }
 
-    const BH = AB - AH
     const aire = (AB * CH) / 2
 
     // Construction de la figure
-    const A = point(0, 0, 'A')
-    const B = point(AB, 0, 'B')
-    const H = point(AH, 0, 'H')
-    const C = point(AH, CH, 'C')
+    const A = pointAbstrait(0, 0, 'A')
+    const B = pointAbstrait(AB, 0, 'B')
+    const H = pointAbstrait(AH, 0, 'H')
+    const C = pointAbstrait(AH, CH, 'C')
 
     const objets = []
     
     // Triangle avec noms
     const pol = polygoneAvecNom(A, B, C)
     objets.push(pol[0], pol[1])
+
     
     // Hauteur [CH]
     objets.push(
       segment(C, H, 'gray'),
-      codageAngleDroit(C, H, A)
+      codageAngleDroit(B,H,C)
     )
 
     // Cotations des longueurs
     objets.push(
       latex2d(
-        `${AC} \\text{ cm}`,
-        milieu(A, C).x - 1,
+        `${texNombre(AC)} \\text{ cm}`,
+        milieu(A, C).x - 2,
         milieu(A, C).y,
         { color: 'black' }
       ),
       latex2d(
-        `${BC} \\text{ cm}`,
-        milieu(B, C).x + 1,
+        `${texNombre(BC)} \\text{ cm}`,
+        milieu(B, C).x + 2,
         milieu(B, C).y,
         { color: 'black' }
       ),
@@ -83,7 +88,7 @@ export default class Can52026Q17 extends ExerciceCan {
         `${CH} \\text{ cm}`,
         milieu(C, H).x + 0.5,
         milieu(C, H).y,
-        { color: 'black' }
+        { color: 'black', orientation: 90 }
       ),
       latex2d(
         `${AB} \\text{ cm}`,
@@ -92,8 +97,10 @@ export default class Can52026Q17 extends ExerciceCan {
         { color: 'black' }
       )
     )
-
-    this.question = mathalea2d(
+objets.push(
+  latex2d('H', H.x-0.5, H.y + 0.5, { color: 'black' })
+)
+    this.canEnonce = 'L\'aire du triangle $ABC$ est égale à : ' + mathalea2d(
       {
         xmin: -2,
         ymin: -2,
@@ -107,13 +114,24 @@ export default class Can52026Q17 extends ExerciceCan {
       objets,
     )
 
-    this.question += "<br>L'aire du triangle ABC est égale à :"
-
+    this.question = 'L\'aire du triangle ABC est égale à :'+ mathalea2d(
+      {
+        xmin: -2,
+        ymin: -2,
+        xmax: AB + 2,
+        ymax: CH + 2,
+        pixelsParCm: 20,
+        mainlevee: false,
+        scale: 0.7,
+        style: 'margin: auto',
+      },
+      objets,
+    )
+this.formatChampTexte = KeyboardType.clavierDeBase
     this.correction = `L'aire du triangle $ABC$ est égale à :<br>
 $\\mathcal{A}=\\dfrac{AB \\times CH}{2}=\\dfrac{${AB} \\times ${CH}}{2}=\\dfrac{${AB * CH}}{2}=${miseEnEvidence(aire)}\\text{ cm}^2$`
 
     this.reponse = aire
-    this.canEnonce = "L'aire du triangle ABC est égale à :"
     this.canReponseACompleter = '$\\ldots \\text{ cm}^2$'
     
     if (this.interactif) {
@@ -122,6 +140,6 @@ $\\mathcal{A}=\\dfrac{AB \\times CH}{2}=\\dfrac{${AB} \\times ${CH}}{2}=\\dfrac{
   }
 
   nouvelleVersion() {
-    this.canOfficielle ? this.enonce(10, 4, 6, 5, 8) : this.enonce()
+    this.canOfficielle ? this.enonce(10, 4, 3, 5, 8) : this.enonce()
   }
 }
