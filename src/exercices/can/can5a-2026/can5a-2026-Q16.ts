22f2b6f768c24a2fd554f12f900597f79a8ec7b5
gillesMora <gilles.mora@ac-normandie.fr>
2026-01-17 18:39:16 +0100

    a relire et champs Q29 et 16 a revoir

--- src/exercices/can/can5a-2026/can5a-2026-Q16.ts
+++ src/exercices/can/can5a-2026/can5a-2026-Q16.ts
@@ -1,18 +1,16 @@
-import { cercle, cercleCentrePoint } from '../../../lib/2d/cercle'
-import { colorToLatexOrHTML } from '../../../lib/2d/colorToLatexOrHtml'
+
+import { fixeBordures } from '../../../lib/2d/fixeBordures'
+import { plot } from '../../../lib/2d/Plot'
 import { pointAbstrait } from '../../../lib/2d/PointAbstrait'
-import { polygone } from '../../../lib/2d/polygones'
 import { segment } from '../../../lib/2d/segmentsVecteurs'
 import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { choice } from '../../../lib/outils/arrayOutils'
 import { miseEnEvidence } from '../../../lib/outils/embellissements'
-import { texNombre } from '../../../lib/outils/texNombre'
 import FractionEtendue from '../../../modules/FractionEtendue'
 import { mathalea2d } from '../../../modules/mathalea2d'
-import { randint } from '../../../modules/outils'
 import ExerciceCan from '../../ExerciceCan'
 
-export const titre = 'Calculer le périmètre d\'un rectangle'
+export const titre = 'Calculer la proportion de boules blanches dans un sac'
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const uuid = 'jjs2e'
@@ -26,7 +24,7 @@ export const refs = {
 
 */
 export default class Can52026Q16 extends ExerciceCan {
-  enonce(nbBlanches?: number, nbNoires?: number) {
+ enonce(nbBlanches?: number, nbNoires?: number) {
     if (nbBlanches == null || nbNoires == null) {
       const listeCas = [
         [3, 5], [4, 4], [2, 6], [5, 3], [3, 6],
@@ -41,10 +39,13 @@ export default class Can52026Q16 extends ExerciceCan {
     const fraction = new FractionEtendue(nbBlanches, total)
     const fractionSimplifiee = fraction.simplifie()
 
-    const boiteGauche = 1
-    const boiteDroite = 4
+    const objets = []
+    
+    // Création du sac (contour)
+    const boiteGauche = 0
+    const boiteDroite = 2.5
     const boiteBas = 0
-    const boiteHaut = 3
+    const boiteHaut = 2.5
 
     const coinBasGauche = pointAbstrait(boiteGauche, boiteBas)
     const coinBasDroit = pointAbstrait(boiteDroite, boiteBas)
@@ -55,65 +56,56 @@ export default class Can52026Q16 extends ExerciceCan {
     const coteBas = segment(coinBasGauche, coinBasDroit)
     const coteDroit = segment(coinBasDroit, coinHautDroit)
 
-    const objets = []
     objets.push(coteGauche, coteBas, coteDroit)
 
-    let compteur = 0
-    const rayonBoule = 0.25
-    const espacementX = 0.6
-    const espacementY = 0.6
-    const margeBas = 0.5
-    const margeGauche = 1.3
-
-    for (let ligne = 0; ligne < 5 && compteur < total; ligne++) {
-      const boulesParLigne = ligne < 4 ? Math.min(4, total - compteur) : total - compteur
-      for (let col = 0; col < boulesParLigne && compteur < total; col++) {
-        const x = margeGauche + col * espacementX
-        const y = margeBas + ligne * espacementY
-        const centre = pointAbstrait(x, y)
-        const boule = cercle(centre, rayonBoule)
-        
-        boule.epaisseur = 2
-        boule.color = colorToLatexOrHTML('black')
-        
-        if (compteur < nbBlanches) {
-          // Boules blanches : pas de remplissage
-          boule.opaciteDeRemplissage = 0
-        } else {
-          // Boules noires : remplissage noir
-          boule.opaciteDeRemplissage = 1
-        }
-        
-        objets.push(boule)
-        compteur++
+    // Création des boules avec plot() - POSITIONNÉES EN BAS
+    const k = 4 // nombre de boules par ligne maximum
+    
+    for (let n = 0; n < total; n++) {
+      const x = n % k
+      const y = Math.floor(n / k)
+      
+      // Position depuis le bas de la boîte
+      const posX = x * 0.6 + 0.3
+      const posY = y * 0.6 + 0.3
+      
+      if (n < nbBlanches) {
+        // Boule blanche : contour noir, pas de remplissage
+        objets.push(
+          plot(posX, posY, {
+            rayon: 0.2,
+            couleur: 'black',
+            couleurDeRemplissage: 'white',
+          })
+        )
+      } else {
+        // Boule noire : contour noir, remplissage noir
+        objets.push(
+          plot(posX, posY, {
+            rayon: 0.2,
+            couleur: 'black',
+            couleurDeRemplissage: 'black',
+          })
+        )
       }
     }
 
     this.canEnonce = mathalea2d(
-      {
-        xmin: 0,
-        ymin: -0.5,
-        xmax: 5,
-        ymax: 3.5,
-        pixelsParCm: 30,
-        scale: 0.8,
-        style: 'margin: auto',
-      },
-      objets,
-    )
-
+      Object.assign({ scale: 0.5 }, fixeBordures(objets)),
+      objets
+    )+'La proportion de boules blanches de cette boîte est :'
     this.question = this.canEnonce
-    this.question += '<br>La proportion de boules blanches de ce sac est :'
+   
 
     this.correction = `Il y a $${nbBlanches}$ boules blanches sur un total de $${total}$ boules.<br>
 La proportion de boules blanches est donc : $\\dfrac{${nbBlanches}}{${total}}=${miseEnEvidence(fractionSimplifiee.texFraction)}$.`
-
+ this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
     this.reponse = fractionSimplifiee.texFraction
     this.canReponseACompleter = '$\\dfrac{\\ldots}{\\ldots}$'
-    this.optionsDeComparaison = { fractionIrreductible: true }
+    
   }
 
   nouvelleVersion() {
-    this.canOfficielle ? this.enonce(3, 5) : this.enonce()
+    this.canOfficielle ? this.enonce(3, 4) : this.enonce()
   }
 }
