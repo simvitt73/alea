77f7c46eb27a2eca91198cd30565fff136134075
@gainmireille <mireille-m.gain@ac-bordeaux.fr>
2026-01-18 18:33:58 +0100

    Correctifs de formes

--- src/lib/problems/problemesComparaisonAdditive/problemesCompAddPteQuantite/coureur.ts
+++ src/lib/problems/problemesComparaisonAdditive/problemesCompAddPteQuantite/coureur.ts
@@ -18,7 +18,7 @@ export function coureur6(decimal = true): Probleme {
   const enonce = `${personnage.prenom} a réalisé un trail au cours duquel ${personnage.pronom} a marché sur $${texNombre(nb2 + nb1, 3)}$ ${uniteComplete}, soit $${texNombre(data.nb2, 3)}$ ${uniteComplete} de plus que la distance courue.
   Sur quelle distance a-t-${personnage.pronom} couru ?`
   const correction = `${personnage.prenom} a marché sur $${texNombre(nb2, 3)}$ ${uniteComplete} de moins que $${texNombre(nb1 + nb2, 3)}$ ${uniteComplete}.
- La distance sur laquelle ${personnage.prenom} a donc couru est : $${texNombre(nb1 + nb2, 3)}\\text{  ${unite}}-${texNombre(nb2, 3)}\\text{  ${unite}} = ${miseEnEvidence(texNombre(nb1, 3))}\\text{  ${unite}}$.`
+ <br>La distance sur laquelle ${personnage.prenom} a donc couru est : $${texNombre(nb1 + nb2, 3)}\\text{  ${unite}}-${texNombre(nb2, 3)}\\text{  ${unite}} = ${miseEnEvidence(texNombre(nb1, 3))}\\text{  ${unite}}$.`
   const probleme = new ProblemeCompAddPteQantite('coureur6', data)
   probleme.enonce = enonce
   probleme.correction = correction
