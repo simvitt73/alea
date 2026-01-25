697effa7f30822d70168ac7d6b2a133ed36708a9
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-01-25 19:37:52 +0100

    Question 30 Ok

--- src/exercices/can/can2a-2026/can2a-2026-Q30.ts
+++ src/exercices/can/can2a-2026/can2a-2026-Q30.ts
@@ -1,17 +1,18 @@
-import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
-import { miseEnEvidence } from '../../../lib/outils/embellissements'
-import { randint } from '../../../modules/outils'
-import ExerciceCan from '../../ExerciceCan'
-import { mathalea2d } from '../../../modules/mathalea2d'
+import { grille } from '../../../lib/2d/Grille'
 import { pointAbstrait } from '../../../lib/2d/PointAbstrait'
+import { polyline } from '../../../lib/2d/Polyline'
 import { segment } from '../../../lib/2d/segmentsVecteurs'
 import { latex2d } from '../../../lib/2d/textes'
-import FractionEtendue from '../../../modules/FractionEtendue'
 import { milieu } from '../../../lib/2d/utilitairesPoint'
-import { polyline } from '../../../lib/2d/Polyline'
+import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
 import { choice } from '../../../lib/outils/arrayOutils'
-import { grille } from '../../../lib/2d/Grille'
-export const titre = 'Multiplier un entier avec un décimal'
+import { miseEnEvidence } from '../../../lib/outils/embellissements'
+import FractionEtendue from '../../../modules/FractionEtendue'
+import { mathalea2d } from '../../../modules/mathalea2d'
+import { randint } from '../../../modules/outils'
+import type { NestedObjetMathalea2dArray } from '../../../types/2d'
+import ExerciceCan from '../../ExerciceCan'
+export const titre = "Déterminer la longueur d'une ligne brisée"
 export const interactifReady = true
 export const interactifType = 'mathLive'
 export const uuid = 'ehwt2'
@@ -19,142 +20,161 @@ export const refs = {
   'fr-fr': [],
   'fr-ch': ['NR'],
 }
+const creerQuestionLigneBrisee = () => {
+  // Choix aléatoire de la longueur de l'unité (en carreaux)
+  const longueurUnite = choice([3, 4, 5, 6])
+
+  // Définir les limites de la grille
+  const xmin = -1
+  const ymin = -2
+  const xmax = 7
+  const ymax = 5
+
+  // Créer la grille
+  const grilleObj = grille(-2, ymin, xmax, ymax - 1, 'gray', 1, 1)
+
+  // Créer le segment unité en haut
+  const G = pointAbstrait(0, 4, 'G', 'above')
+  const H = pointAbstrait(longueurUnite, 4, 'H', 'above')
+  const segmentUnite = segment(G, H)
+  segmentUnite.epaisseur = 3
+
+  // Générer aléatoirement une ligne brisée
+  // La ligne brisée aura entre longueurUnite et 3*longueurUnite carreaux de longueur
+  const longueurLigneBrisee = randint(longueurUnite + 1, 3 * longueurUnite)
+
+  // Créer les points de la ligne brisée
+  // On commence à (0, 2) et on crée des segments horizontaux et verticaux
+  const points = [pointAbstrait(0, 2)]
+  let xActuel = 0
+  let yActuel = 2
+  let longueurRestante = longueurLigneBrisee
+  const xMaxLigne = 6 // Limite horizontale pour la ligne brisée
+  let longueurReelle = 0 // Longueur réelle de la ligne brisée créée
+
+  while (longueurRestante > 0) {
+    // Alterner entre segments horizontaux et verticaux
+    if (points.length % 2 === 1) {
+      // Segment horizontal
+      // Calculer la longueur maximale possible sans sortir de la grille
+      const longueurMax = Math.min(xMaxLigne - xActuel, longueurRestante)
+      if (longueurMax <= 0) {
+        // Si on ne peut plus avancer horizontalement, on arrête
+        break
+      }
+      const longueur = Math.min(randint(1, 3), longueurMax)
+      xActuel += longueur
+      points.push(pointAbstrait(xActuel, yActuel))
+      longueurRestante -= longueur
+      longueurReelle += longueur
+    } else {
+      // Segment vertical
+      const direction = choice([-1, 1])
+      const longueur = Math.min(randint(1, 2), longueurRestante)
+      const nouveauY = yActuel + direction * longueur
+      // Limiter y entre 0 et 3 et calculer la longueur effective
+      const yFinal = Math.max(0, Math.min(3, nouveauY))
+      const longueurEffective = Math.abs(yFinal - yActuel)
+      yActuel = yFinal
+      points.push(pointAbstrait(xActuel, yActuel))
+      longueurRestante -= longueurEffective
+      longueurReelle += longueurEffective
+    }
+  }
+  const ligneBrisee = polyline(...points)
+  ligneBrisee.epaisseur = 3
+
+  // Assembler les objets
+  const objets = []
+  objets.push(
+    latex2d('1 u.\\ell.', milieu(G, H).x, milieu(G, H).y + 0.5, {
+      letterSize: 'normalsize',
+    }),
+    grilleObj,
+    segmentUnite,
+    ligneBrisee,
+  )
+
+  return {
+    objets,
+    longueurUnite,
+    longueurLigneBrisee: longueurReelle, // Retourner la longueur réelle
+    xmin,
+    ymin,
+    xmax,
+    ymax,
+  }
+}
 /**
  * Modèle d'exercice très simple pour la course aux nombres
  * @author Gilles Mora
 
 */
 export default class Can2a2026Q30 extends ExerciceCan {
- enonce(): void {
+  enonce(): void {
     this.formatChampTexte = KeyboardType.clavierDeBaseAvecFraction
     this.optionsChampTexte = { texteAvant: ' ', texteApres: 'u.$\\ell$' }
-  
+    let questionData
     // Fonction pour créer aléatoirement une question de ligne brisée sur grille
-            const creerQuestionLigneBrisee = () => {
-              // Choix aléatoire de la longueur de l'unité (en carreaux)
-              const longueurUnite = choice([3, 4, 5, 6])
+    if (this.canOfficielle || this.sup) {
+      const objets: NestedObjetMathalea2dArray = []
+      objets.push(grille(0, 0, 8, 6, 'gray', 1, 1))
+      const G = pointAbstrait(1, 6)
+      const H = pointAbstrait(6, 6)
+      const segmentUnite = segment(G, H)
+      segmentUnite.epaisseur = 3
+      objets.push(segmentUnite)
+      objets.push(
+        latex2d('1 u.\\ell.', milieu(G, H).x, milieu(G, H).y + 0.5, {
+          letterSize: 'normalsize',
+        }),
+      )
+      const ligneBrisee = polyline(
+        pointAbstrait(1, 4),
+        pointAbstrait(4, 4),
+        pointAbstrait(4, 2),
+        pointAbstrait(7, 2),
+      )
+      ligneBrisee.epaisseur = 3
+      objets.push(ligneBrisee)
+      questionData = {
+        objets,
+        longueurUnite: 5,
+        longueurLigneBrisee: 8,
+        xmin: 0,
+        ymin: 0,
+        xmax: 8,
+        ymax: 7,
+      }
+    } else {
+      questionData = creerQuestionLigneBrisee()
+    }
+    const b = questionData.longueurUnite
+    const longueurCarreaux = questionData.longueurLigneBrisee
 
-              // Définir les limites de la grille
-              const xmin = -1
-              const ymin = -2
-              const xmax = 7
-              const ymax = 5
+    this.reponse = new FractionEtendue(longueurCarreaux, b)
 
-              // Créer la grille
-              const grilleObj = grille(-2, ymin, xmax, ymax - 1, 'gray', 1, 1)
+    this.question = 'Longueur de la ligne brisée en u.$\\ell$.<br>'
+    this.question +=
+      mathalea2d(
+        {
+          xmin: questionData.xmin,
+          ymin: questionData.ymin,
+          xmax: questionData.xmax,
+          ymax: questionData.ymax,
+          pixelsParCm: 20,
+          mainlevee: false,
+          amplitude: 0.5,
+          scale: 0.5,
+          style: 'margin: auto',
+        },
+        questionData.objets,
+      ) + `<br>`
 
-              // Créer le segment unité en haut
-              const G = pointAbstrait(0, 4, 'G', 'above')
-              const H = pointAbstrait(longueurUnite, 4, 'H', 'above')
-              const segmentUnite = segment(G, H)
-              segmentUnite.epaisseur = 2
+    this.correction = `Une unité correspond à $${b}$ carreaux, la ligne brisée mesure $${longueurCarreaux}$ carreaux, soit $${miseEnEvidence(this.reponse.texFraction)}$ u.l. `
 
-              // Générer aléatoirement une ligne brisée
-              // La ligne brisée aura entre longueurUnite et 3*longueurUnite carreaux de longueur
-              const longueurLigneBrisee = randint(
-                longueurUnite + 1,
-                3 * longueurUnite,
-              )
-
-              // Créer les points de la ligne brisée
-              // On commence à (0, 2) et on crée des segments horizontaux et verticaux
-              const points = [pointAbstrait(0, 2)]
-              let xActuel = 0
-              let yActuel = 2
-              let longueurRestante = longueurLigneBrisee
-              const xMaxLigne = 6 // Limite horizontale pour la ligne brisée
-              let longueurReelle = 0 // Longueur réelle de la ligne brisée créée
-
-              while (longueurRestante > 0) {
-                // Alterner entre segments horizontaux et verticaux
-                if (points.length % 2 === 1) {
-                  // Segment horizontal
-                  // Calculer la longueur maximale possible sans sortir de la grille
-                  const longueurMax = Math.min(
-                    xMaxLigne - xActuel,
-                    longueurRestante,
-                  )
-                  if (longueurMax <= 0) {
-                    // Si on ne peut plus avancer horizontalement, on arrête
-                    break
-                  }
-                  const longueur = Math.min(randint(1, 3), longueurMax)
-                  xActuel += longueur
-                  points.push(pointAbstrait(xActuel, yActuel))
-                  longueurRestante -= longueur
-                  longueurReelle += longueur
-                } else {
-                  // Segment vertical
-                  const direction = choice([-1, 1])
-                  const longueur = Math.min(randint(1, 2), longueurRestante)
-                  const nouveauY = yActuel + direction * longueur
-                  // Limiter y entre 0 et 3 et calculer la longueur effective
-                  const yFinal = Math.max(0, Math.min(3, nouveauY))
-                  const longueurEffective = Math.abs(yFinal - yActuel)
-                  yActuel = yFinal
-                  points.push(pointAbstrait(xActuel, yActuel))
-                  longueurRestante -= longueurEffective
-                  longueurReelle += longueurEffective
-                }
-              }
-              const ligneBrisee = polyline(...points)
-              ligneBrisee.epaisseur = 2
-
-              // Assembler les objets
-              const objets = []
-              objets.push(
-                latex2d(
-                  '1 u.\\ell.',
-                  milieu(G, H).x,
-                  milieu(G, H).y + 0.7,
-                  { letterSize: 'scriptsize' },
-                ),
-                grilleObj,
-                segmentUnite,
-                ligneBrisee,
-              )
-
-              return {
-                objets,
-                longueurUnite,
-                longueurLigneBrisee: longueurReelle, // Retourner la longueur réelle
-                xmin,
-                ymin,
-                xmax,
-                ymax,
-              }
-            }
-
-            const questionData = creerQuestionLigneBrisee()
-           const b = questionData.longueurUnite
-            const longueurCarreaux = questionData.longueurLigneBrisee
-
-            this.reponse = new FractionEtendue(longueurCarreaux, b)
-
-            this.question =
-              'Longueur de la ligne brisée en u.$\\ell$.<br>'
-            this.question +=
-              mathalea2d(
-                {
-                  xmin: questionData.xmin,
-                  ymin: questionData.ymin,
-                  xmax: questionData.xmax,
-                  ymax: questionData.ymax,
-                  pixelsParCm: 20,
-                  mainlevee: false,
-                  amplitude: 0.5,
-                  scale: 0.5,
-                  style: 'margin: auto',
-                },
-                questionData.objets,
-              ) + `<br>`
-
-            this.correction = `Une unité correspond à $${b}$ carreaux, la ligne brisée mesure $${longueurCarreaux}$ carreaux, soit $${miseEnEvidence(this.reponse.texFraction)}$ u.l. `
-
-            this.canEnonce = this.question
-            this.canReponseACompleter = '$\\ldots$ u.$\\ell$.'
-           
-            
+    this.canEnonce = this.question
+    this.canReponseACompleter = '$\\ldots$ u.$\\ell$.'
   }
 
   nouvelleVersion(): void {
