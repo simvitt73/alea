01b58864ebf7d5fbecdb65ad138a0e3a94add157
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-01-28 22:09:09 +0100

    Tranformation de can2F25 en exercice de type simple

--- src/exercices/can/2e/can2F25.ts
+++ src/exercices/can/2e/can2F25.ts
@@ -1,7 +1,5 @@
 import { fixeBordures } from '../../../lib/2d/fixeBordures'
 import { MetaInteractif2d } from '../../../lib/2d/interactif2d'
-import { KeyboardType } from '../../../lib/interactif/claviers/keyboard'
-import { handleAnswers } from '../../../lib/interactif/gestionInteractif'
 import { ajouteFeedback } from '../../../lib/interactif/questionMathLive'
 import {
   creerTableauHtml,
@@ -11,7 +9,7 @@ import { choice } from '../../../lib/outils/arrayOutils'
 import { texFractionReduite } from '../../../lib/outils/deprecatedFractions'
 import { reduireAxPlusB } from '../../../lib/outils/ecritures'
 import { mathalea2d } from '../../../modules/mathalea2d'
-import { listeQuestionsToContenu, randint } from '../../../modules/outils'
+import { randint } from '../../../modules/outils'
 import Exercice from '../../Exercice'
 
 export const titre = 'Dresser le tableau de signes d’une fonction affine'
@@ -36,157 +34,128 @@ export default class TableauSignes extends Exercice {
   constructor() {
     super()
     this.nbQuestions = 1
+    this.typeExercice = 'simple'
+    this.formatInteractif = 'MetaInteractif2d'
   }
 
   nouvelleVersion() {
-    for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50; ) {
-      const a = randint(1, 6) * choice([-1, 1]) // coefficient a de la fonction affine
-      const b = randint(1, 6) * choice([-1, 1]) // coefficient b de la fonction affine
-      const zero = texFractionReduite(-b, a)
+    const a = randint(1, 6) * choice([-1, 1]) // coefficient a de la fonction affine
+    const b = randint(1, 6) * choice([-1, 1]) // coefficient b de la fonction affine
+    const zero = texFractionReduite(-b, a)
 
-      let texte = `Compléter le tableau de signes de la fonction $f$ définie sur $\\mathbb{R}$ par $f(x)=${reduireAxPlusB(a, b)}$.`
+    let texte = `Compléter le tableau de signes de la fonction $f$ définie sur $\\mathbb{R}$ par $f(x)=${reduireAxPlusB(a, b)}$.`
 
-      let texteCorr = `$f$ est une fonction affine. Elle s'annule en $x_0=${zero}$. `
+    let texteCorr = `$f$ est une fonction affine. Elle s'annule en $x_0=${zero}$. `
 
-      if (a > 0) {
-        texteCorr += `<br>Comme $${a}>0~$, $~f(x)$ est positif pour $~x>${zero} ~$ et négatif pour $~x<${zero} $.<br>`
-      } else {
-        texteCorr += `<br>Comme $${a}<0$,  $f(x)~$ est négatif pour $~x>${zero} ~$ et positif pour $~x<${zero} $.<br>`
-      }
+    if (a > 0) {
+      texteCorr += `<br>Comme $${a}>0~$, $~f(x)$ est positif pour $~x>${zero} ~$ et négatif pour $~x<${zero} $.<br>`
+    } else {
+      texteCorr += `<br>Comme $${a}<0$,  $f(x)~$ est négatif pour $~x>${zero} ~$ et positif pour $~x<${zero} $.<br>`
+    }
 
-      let ligne1
-      if (a > 0) {
-        ligne1 = ['Line', 25, '', 0, '-', 20, 'z', 20, '+']
-      } else {
-        ligne1 = ['Line', 25, '', 0, '+', 20, 'z', 20, '-']
-      }
+    let ligne1
+    if (a > 0) {
+      ligne1 = ['Line', 25, '', 0, '-', 20, 'z', 20, '+']
+    } else {
+      ligne1 = ['Line', 25, '', 0, '+', 20, 'z', 20, '-']
+    }
 
-      texteCorr += " D'où le tableau de signes suivant :<br>"
-      texteCorr += tableauDeVariation({
-        tabInit: [
-          [
-            ['$x$', 3, 25],
-            [`$f(x)=${reduireAxPlusB(a, b)}$`, 2, 50],
-          ],
-          [
-            '$-\\infty$',
-            20,
-            `$${zero}$`,
-            20,
-            '$+\\infty$',
-            30,
-          ],
-        ],
-        tabLines: [ligne1],
-        colorBackground: '',
-        espcl: 3.5,
-        deltacl: 0.8,
-        lgt: 8,
-        hauteurLignes: [15, 15],
-      })
-
-      // Création du tableau interactif à compléter
-      const objetsTableau = creerTableauHtml({
-        tabInit: [
-          [
-            ['$x$', 3, 25],
-            [`$f(x)=${reduireAxPlusB(a, b)}$`, 2, 50],
-          ],
-          [
-            '$-\\infty$',
-            20,
-            '',
-            20,
-            '$+\\infty$',
-            30,
-          ],
-        ],
-        tabLines: [['Line', 25, '', 0, '', 20, 'z', 20, '']],
-        colorBackground: '',
-        espcl: 3.5,
-        deltacl: 0.8,
-        lgt: 8,
-      })
-
-      // Création des champs interactifs
-      const input = new MetaInteractif2d(
+    texteCorr += " D'où le tableau de signes suivant :<br>"
+    texteCorr += tableauDeVariation({
+      tabInit: [
         [
-          {
-            x: 12.2,
-            y: -1.5,
-            content: '%{champ1}',
-            classe: '',
-            blanc: '\\ldots',
-            opacity: 1,
-            index: 0,
-          },
-          {
-            x: 10,
-            y: -4,
-            content: '%{champ1}',
-            classe: '',
-            blanc: '\\ldots',
-            opacity: 1,
-            index: 1,
-          },
-          {
-            x: 14.4,
-            y: -4,
-            content: '%{champ1}',
-            classe: '',
-            blanc: '\\ldots',
-            opacity: 1,
-            index: 2,
-          },
+          ['$x$', 3, 25],
+          [`$f(x)=${reduireAxPlusB(a, b)}$`, 2, 50],
         ],
+        ['$-\\infty$', 20, `$${zero}$`, 20, '$+\\infty$', 30],
+      ],
+      tabLines: [ligne1],
+      espcl: 3.5,
+      deltacl: 0.8,
+      lgt: 8,
+      hauteurLignes: [15, 15],
+    })
+
+    // Création du tableau interactif à compléter
+    const objetsTableau = creerTableauHtml({
+      tabInit: [
+        [
+          ['$x$', 3, 25],
+          [`$f(x)=${reduireAxPlusB(a, b)}$`, 2, 50],
+        ],
+        ['$-\\infty$', 20, '', 20, '$+\\infty$', 30],
+      ],
+      tabLines: [['Line', 25, '', 0, '', 20, 'z', 20, '']],
+      espcl: 3.5,
+      deltacl: 0.8,
+      lgt: 8,
+    })
+
+    // Création des champs interactifs
+    const input = new MetaInteractif2d(
+      [
         {
-          exercice: this,
-          question: i,
+          x: 12.2,
+          y: -1.5,
+          content: '%{champ1}',
+          classe: '',
+          blanc: '\\ldots',
+          opacity: 1,
+          index: 0,
         },
-      )
-      objetsTableau.push(input)
-
-      // Gestion des réponses
-      handleAnswers(
-        this,
-        i,
         {
-          field0: { value: zero },
-          field1: { value: a > 0 ? '-' : '+' },
-          field2: { value: a > 0 ? '+' : '-' },
+          x: 10,
+          y: -4,
+          content: '%{champ1}',
+          classe: '',
+          blanc: '\\ldots',
+          opacity: 1,
+          index: 1,
         },
-        { formatInteractif: 'MetaInteractif2d' },
-      )
+        {
+          x: 14.4,
+          y: -4,
+          content: '%{champ1}',
+          classe: '',
+          blanc: '\\ldots',
+          opacity: 1,
+          index: 2,
+        },
+      ],
+      {
+        exercice: this,
+        question: 0,
+      },
+    )
+    objetsTableau.push(input)
 
-      // Ajout du tableau avec interactivité
-      texte +=
-        mathalea2d(
-          Object.assign({}, fixeBordures(objetsTableau)),
-          objetsTableau,
-        ) +
-        (this.interactif
-          ? `<span id="resultatCheckEx${this.numeroExercice}Q${i}"></span>` +
-            ajouteFeedback(this, i)
-          : '')
+    this.reponse = {
+      field0: { value: zero },
+      field1: { value: a > 0 ? '-' : '+' },
+      field2: { value: a > 0 ? '+' : '-' },
+    }
 
-      if (this.questionJamaisPosee(i, a, b)) {
-        this.listeQuestions[i] = texte
-        this.listeCorrections[i] = texteCorr
+    // Ajout du tableau avec interactivité
+    texte +=
+      mathalea2d(
+        Object.assign({}, fixeBordures(objetsTableau)),
+        objetsTableau,
+      ) +
+      (this.interactif
+        ? `<span id="resultatCheckEx${this.numeroExercice}Q${0}"></span>` +
+          ajouteFeedback(this, 0)
+        : '')
 
-        this.canEnonce = `Dresser le tableau de signes de la fonction $f$ définie sur  $\\mathbb{R}$ par $f(x)=${reduireAxPlusB(a, b)}$. `
-        this.canReponseACompleter = `$\\begin{array}{|c|ccc|}
+    this.question = texte
+    this.correction = texteCorr
+
+    this.canEnonce = `Dresser le tableau de signes de la fonction $f$ définie sur  $\\mathbb{R}$ par $f(x)=${reduireAxPlusB(a, b)}$. `
+    this.canReponseACompleter = `$\\begin{array}{|c|ccc|}
 \\hline
 x & \\scriptstyle{-\\infty} & \\ldots\\ldots & \\scriptstyle{+\\infty} \\\\
 \\hline
 f(x) & \\ldots\\ldots & \\scriptstyle{0} & \\ldots\\ldots \\\\
 \\hline
 \\end{array}$`
-        this.listeCanEnonces.push(this.canEnonce)
-        this.listeCanReponsesACompleter.push(this.canReponseACompleter)
-        i++
-      }
-      cpt++
-    }
-    listeQuestionsToContenu(this)
   }
 }
