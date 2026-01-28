9426374ede1213ca7c55e6d3f4f6567f41dc1209
lhotejc <jean-claude.lhote@ac-nancy-metz.fr>
2026-01-28 21:07:33 +0100

    Nouveau 2F30-1 avec amélioration de courbe

--- src/exercices/2e/2F30-1-old.ts
+++ src/exercices/2e/2F30-1-old.ts
@@ -8,9 +8,7 @@ import {
   tableauDeVariation,
   tableauVariationsFonction,
 } from '../../lib/mathFonctions/etudeFonction'
-import { choice } from '../../lib/outils/arrayOutils'
-import { ecritureAlgebrique, rienSi1 } from '../../lib/outils/ecritures'
-import { texNombre } from '../../lib/outils/texNombre'
+import { choice, combinaisonListes } from '../../lib/outils/arrayOutils'
 import type FractionEtendue from '../../modules/FractionEtendue'
 import { mathalea2d } from '../../modules/mathalea2d'
 
@@ -29,11 +27,11 @@ export const dateDeModifImportante = '28/02/2025'
  * @author Gilles Mora
 
  */
-export const uuid = '05b53'
+export const uuid = '05b52'
 
 export const refs = {
-  'fr-fr': ['2F30-1'],
-  'fr-ch': ['2mFctPoly-4'],
+  'fr-fr': [],
+  'fr-ch': [],
 }
 export default class VariationsCourbe extends Exercice {
   constructor() {
@@ -47,22 +45,25 @@ export default class VariationsCourbe extends Exercice {
         '1 : Intervalle borné',
         '2 : Intervalle non borné à gauche ou à droite',
         '3 : Sur R',
-        '4 : Fonction avec valeurs interdites',
-        '0: Mélange',
+        '4: Mélange',
       ].join('\n'),
     ]
   }
 
   nouvelleVersion() {
-    const listeTypeDeQuestions = gestionnaireFormulaireTexte({
+    const typesDeQuestionsDisponibles = gestionnaireFormulaireTexte({
       saisie: this.sup,
       min: 1,
-      max: 4,
-      melange: 0,
-      defaut: 0,
+      max: 3,
+      melange: 4,
+      defaut: 4,
       nbQuestions: this.nbQuestions,
-    }).map(Number)
+    })
 
+    const listeTypeDeQuestions = combinaisonListes(
+      typesDeQuestionsDisponibles,
+      this.nbQuestions,
+    )
     for (let i = 0, cpt = 0; i < this.nbQuestions && cpt < 50; ) {
       const nomF = choice([['f'], ['g'], ['h'], ['u'], ['v'], ['w']])
       const nom = choice(nomF)
@@ -1424,6 +1425,7 @@ export default class VariationsCourbe extends Exercice {
           break
 
         case 3:
+        default:
           choix = randint(1, 3)
 
           if (choix === 1) {
@@ -1643,192 +1645,6 @@ export default class VariationsCourbe extends Exercice {
             texteCorr = `La fonction $f$ est définie sur $\\mathbb{R}$.<br>
          Son tableau de variations est : <br><br>`
             texteCorr += `${tableau}`
-          }
-          break
-        case 4:
-        default:
-          {
-            const genereDenomAvecPoles = (nbPoles: number) => {
-              let a = randint(-8, -2) / 2
-              const b = randint(2, 8) / 2
-              const a1 = randint(-3, 3, [a, -a, 0])
-              const b1 = randint(-3, 3, [-a1, 0])
-              if (-a * a1 - b1 === 0) {
-                a -= 1
-              }
-              const fonction =
-                nbPoles === 2
-                  ? (x: number) =>
-                      (10 * ((a1 * x) / 10 + b1 / 10)) / ((x - b) * (x - a))
-                  : (x: number) => (a1 * x + b1) / (x - a)
-              let denomTex: string
-              if (nbPoles === 2) {
-                denomTex = `(x  ${ecritureAlgebrique(-a)})(x  ${ecritureAlgebrique(-b)})`
-              } else {
-                denomTex = `x  ${ecritureAlgebrique(-a)}`
-              }
-              const fonctionTex =
-                nbPoles === 2
-                  ? `\\dfrac{(${rienSi1(a1)}x ${ecritureAlgebrique(b1)})}{(x  ${ecritureAlgebrique(-a)})(x  ${ecritureAlgebrique(-b)})}`
-                  : `\\dfrac{${rienSi1(a1)}x ${ecritureAlgebrique(b1)}}{x ${ecritureAlgebrique(-a)}}`
-              const numTex = `${rienSi1(a1)}x ${ecritureAlgebrique(b1)}`
-
-              const signes =
-                nbPoles === 2
-                  ? [
-                      a1 > 0 ? '+/ ' : '-/ ',
-                      a1 > 0 ? '-D+/ / ' : '+D-/ / ',
-                      a1 > 0 ? '-D+/ / ' : '+D-/ / ',
-                      a1 > 0 ? '-/ ' : '+/ ',
-                    ]
-                  : [
-                      a1 > 0 ? '-/ ' : '+/ ',
-                      a1 > 0 ? '+D-/ / ' : '-D+/ / ',
-                      a1 > 0 ? '+/ ' : '-/ ',
-                    ]
-              const limiteMoinsInfini = nbPoles === 2 ? 0 : a1
-              const limitePlusInfini = nbPoles === 2 ? 0 : a1
-
-              return {
-                fonction,
-                fonctionTex,
-                denomTex,
-                numTex,
-                signes,
-                limiteMoinsInfini,
-                limitePlusInfini,
-                valeursInterdites:
-                  nbPoles === 2 ? [a, b].sort((a, b) => a - b) : [a],
-              }
-            }
-            choix = 1 // randint(1, 2) // 1 = 1 valeur interdite, 2 = 2 valeurs interdites Pour l'instant on ne fait que des 1 valeur interdite
-            // Le choix 2 est foireux pour l'instant
-            const poles = genereDenomAvecPoles(choix)
-            fonction = poles.fonction
-            const r1 = repere({
-              xMin: -6,
-              yMin: -20,
-              yMax: 20,
-              xMax: 6,
-              xUnite: 1,
-              yUnite: 0.2,
-              xThickDistance: 1,
-              yThickDistance: 10,
-              xLabelMin: -6,
-              xLabelMax: 6,
-              yLabelMin: -20,
-              yLabelMax: 20,
-              yLabelEcart: 0.6,
-              grilleXDistance: 1,
-              grilleYDistance: 1,
-              grilleXMin: -6,
-              grilleYMin: -20,
-              grilleXMax: 6,
-              grilleYMax: 20,
-            })
-            const cf = courbe(fonction, {
-              repere: r1,
-              xMin: -6,
-              xMax: 6,
-              color: 'blue',
-              epaisseur: 2,
-              step: 0.02,
-            })
-            const graphique = mathalea2d(
-              {
-                xmin: -6,
-                xmax: 6,
-                ymin: -4.1,
-                ymax: 4.1,
-                pixelsParCm: 30,
-                scale: 0.6,
-                style: 'margin: auto',
-              },
-              r1,
-              cf,
-            )
-
-            texte = `La fonction $${nom}$ est définie sur $\\mathbb{R} \\setminus \\{${poles.valeursInterdites.map((v) => texNombre(v, 1)).join(' ; ')}\\}$.<br>
-            ${graphique}<br>`
-            texte += `où $${nom}(x) = ${poles.fonctionTex}$.<br>Le dénominateur est $${poles.denomTex}$ et le numérateur est $${poles.numTex}$.<br>`
-            texteCorr = `La fonction $${nom}$ est définie sur $\\mathbb{R} \\setminus \\{${poles.valeursInterdites.map((v) => texNombre(v, 1)).join(' ; ')}\\}$.<br>
-            Son tableau de variations est : <br><br>`
-            if (choix === 2) {
-              texteCorr +=
-                tableauDeVariation({
-                  tabInit: [
-                    [
-                      ['$x$', 1.5, 10],
-                      [`$${nom}(x)$`, 4, 30],
-                    ],
-                    [
-                      `$-\\infty$`,
-                      10,
-                      `${texNombre(poles.valeursInterdites[0], 1)}`,
-                      20,
-                      `${texNombre(poles.valeursInterdites[1], 1)}`,
-                      20,
-                      `$+\\infty$`,
-                      10,
-                    ],
-                  ],
-                  tabLines: [
-                    [
-                      'Var',
-                      20,
-                      `${poles.signes[0]} `,
-                      20,
-                      `${poles.signes[1]}`,
-                      20,
-                      `${poles.signes[2]}`,
-                      20,
-                      `${poles.signes[3]}`,
-                    ],
-                  ],
-                  espcl: 4, // taille en cm entre deux antécédents
-                  deltacl: 1, // distance entre la bordure et les premiers et derniers antécédents
-                  lgt: 2.5, // taille de la première colonne en cm
-                  hauteurLignes: [15, 15],
-                }) + '<br>'
-            } else {
-              texteCorr +=
-                tableauDeVariation({
-                  tabInit: [
-                    [
-                      ['$x$', 1.5, 10],
-                      [`$${nom}(x)$`, 4, 30],
-                    ],
-                    [
-                      `$-\\infty$`,
-                      10,
-                      `${texNombre(poles.valeursInterdites[0], 1)}`,
-                      10,
-                      `$+\\infty$`,
-                      10,
-                    ],
-                  ],
-                  tabLines: [
-                    [
-                      'Var',
-                      10,
-                      `${poles.signes[0]}`,
-                      10,
-                      `${poles.signes[1]}`,
-                      10,
-                      `${poles.signes[2]}`,
-                    ],
-                  ],
-                  espcl: 4, // taille en cm entre deux antécédents
-                  deltacl: 1, // distance entre la bordure et les premiers et derniers antécédents
-                  lgt: 2.5, // taille de la première colonne en cm
-                  hauteurLignes: [15, 15],
-                }) + '<br>'
-            }
-
-            variables.push(
-              ...poles.valeursInterdites,
-              ...poles.signes.map((s) => (s === '+' ? 1 : -1)),
-            )
           }
           break
       }
